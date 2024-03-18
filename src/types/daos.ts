eb21e68695cb09104b63f0f0f126959a6d5eb600
cristianizzo <cristianizzo@me.com>
2024-03-18 16:36:06 +0100

    unit test sync

--- src/types/daos.ts
+++ src/types/daos.ts
@@ -53,7 +53,7 @@ export interface IDao extends IDaoMetadata {
   proposalsCreated: number
   proposalsExecuted: number
   tvlUSD: number
-  txHash: HexAddress
+  txHash: HexAddress | null
   uniqueVoters: number
   votes: number
   hideDao: boolean
