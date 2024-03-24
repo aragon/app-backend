4d47a5c467f7401a92502008787fbf36ed7ac66b
cristianizzo <cristianizzo@me.com>
2024-03-24 23:54:19 +0100

    prettier and increase coverage

--- test/unit/services/api/routers/dao.spec.ts
+++ test/unit/services/api/routers/dao.spec.ts
@@ -16,33 +16,65 @@ describe('Router: Dao', () => {
     sandbox?.restore()
   })
 
-  it('Should get dao with pagination', async () => {
-    const params = {
-      network: NetworksEnum.ethereum,
-      plugin: EnumPluginType.MultisigPlugin,
-      limit: 10,
-      offset: 1,
-      order: 'asc',
-      orderProp: 'createdAt',
-    }
+  describe('getWithPagination', async () => {
+    it('Should get dao with pagination - all params', async () => {
+      const params = {
+        network: NetworksEnum.ethereum,
+        plugin: EnumPluginType.MultisigPlugin,
+        limit: 10,
+        offset: 1,
+        order: 'asc',
+        orderProp: 'createdAt',
+      }
 
-    const stubCtrl = sandbox.stub(DaoController, 'getWithPagination').returns(true as any)
+      const stubCtrl = sandbox.stub(DaoController, 'getWithPagination').returns(true as any)
 
-    const ctx: any = {
-      query: params,
-    }
+      const ctx: any = {
+        query: params,
+      }
 
-    await DaoRouter.getWithPagination(ctx)
+      await DaoRouter.getWithPagination(ctx)
 
-    expect(ctx.body).to.eq(true)
-    expect(stubCtrl.calledOnce).to.be.true
+      expect(ctx.body).to.eq(true)
+      expect(stubCtrl.calledOnce).to.be.true
 
-    const fakeRes = {
-      ...params,
-      search: undefined,
-      fromDate: undefined,
-      toDate: undefined,
-    }
-    expect(stubCtrl.calledWith(fakeRes as any)).to.be.true
+      const fakeRes = {
+        ...params,
+        search: undefined,
+        fromDate: undefined,
+        toDate: undefined,
+      }
+      expect(stubCtrl.calledWith(fakeRes as any)).to.be.true
+    })
+
+    it('Should get dao with pagination - missing pagination params', async () => {
+      const params = {
+        network: NetworksEnum.ethereum,
+        plugin: EnumPluginType.MultisigPlugin,
+        orderProp: 'createdAt',
+      }
+
+      const stubCtrl = sandbox.stub(DaoController, 'getWithPagination').returns(true as any)
+
+      const ctx: any = {
+        query: params,
+      }
+
+      await DaoRouter.getWithPagination(ctx)
+
+      expect(ctx.body).to.eq(true)
+      expect(stubCtrl.calledOnce).to.be.true
+
+      const fakeRes = {
+        ...params,
+        search: undefined,
+        fromDate: undefined,
+        toDate: undefined,
+        limit: 10,
+        offset: 1,
+        order: 'desc',
+      }
+      expect(stubCtrl.calledWith(fakeRes as any)).to.be.true
+    })
   })
 })
