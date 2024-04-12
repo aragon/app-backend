26f2fbe87d9aa509c25583be36eb369f29246cd7
CryptoCrash <cristianizzo@me.com>
2024-04-12 09:49:37 +0200

    aragon contract configurations (#18)
    * aragon contract configurations
    PluginSetupProcessorABI
    
    * config

--- src/types/config.ts
+++ src/types/config.ts
@@ -12,6 +12,48 @@ export enum IEnumNodeEnv {
   production = 'production',
 }
 
+interface ContractEventFilter {
+  address: string
+  blockNumber: number
+  deploymentTx: string
+}
+
+interface ContractEvent {
+  AddresslistVotingSetup: ContractEventFilter
+  AddresslistVotingSetupImplementation: ContractEventFilter
+  AdminSetup: ContractEventFilter
+  AdminSetupImplementation: ContractEventFilter
+  DAOBase: ContractEventFilter
+  DAOFactory: ContractEventFilter
+  DAORegistryProxy: ContractEventFilter
+  DAORegistryImplementation: ContractEventFilter
+  DAOENSSubdomainRegistrarProxy: ContractEventFilter
+  DAOENSSubdomainRegistrarImplementation: ContractEventFilter
+  GovernanceERC20: ContractEventFilter
+  GovernanceWrappedERC20: ContractEventFilter
+  MultisigSetup: ContractEventFilter
+  MultisigSetupImplementation: ContractEventFilter
+  PluginRepoBase: ContractEventFilter
+  PluginRepoFactory: ContractEventFilter
+  PluginRepoRegistryProxy: ContractEventFilter
+  PluginRepoRegistryImplementation: ContractEventFilter
+  PluginSetupProcessor: ContractEventFilter
+  PluginENSSubdomainRegistrarProxy: ContractEventFilter
+  PluginENSSubdomainRegistrarImplementation: ContractEventFilter
+  TokenVotingSetup: ContractEventFilter
+  TokenVotingSetupImplementation: ContractEventFilter
+  AddresslistVotingRepoProxy: ContractEventFilter
+  AddresslistVotingRepoImplementation: ContractEventFilter
+  AdminRepoProxy: ContractEventFilter
+  AdminRepoImplementation: ContractEventFilter
+  ManagementDAOProxy: ContractEventFilter
+  ManagementDAOImplementation: ContractEventFilter
+  MultisigRepoProxy: ContractEventFilter
+  MultisigRepoImplementation: ContractEventFilter
+  TokenVotingRepoProxy: ContractEventFilter
+  TokenVotingRepoImplementation: ContractEventFilter
+}
+
 export interface IConfig {
   APP_NAME: string
   ENVIRONMENT: IEnumEnvironment
@@ -96,6 +138,32 @@ export interface IConfig {
       TOKEN_FETCH_BATCH_SIZE: number
       DAO_INTERVAL: number
       DAO_FETCH_BATCH_SIZE: number
+    }
+  }
+
+  ARAGON_CONTRACTS: {
+    ARBITRUM: {
+      'v1.3.0': ContractEvent
+    }
+    ARBITRUM_SEPOLIA: {
+      'v1.3.0': ContractEvent
+    }
+    BASE: {
+      'v1.3.0': ContractEvent
+    }
+    BASE_SEPOLIA: {
+      'v1.3.0': ContractEvent
+    }
+    MAINNET: {
+      'v1.0.0': ContractEvent
+      'v1.3.0': ContractEvent
+    }
+    POLYGON: {
+      'v1.0.0': ContractEvent
+      'v1.3.0': ContractEvent
+    }
+    SEPOLIA: {
+      'v1.3.0': ContractEvent
     }
   }
 }
