fbfefc4167a910d94fdf49173c17b2faf9bbae08
cristianizzo <cristianizzo@me.com>
2024-04-12 09:08:08 +0200

    etherscan helper + unit test + manual test

---
+++ test/manual/etherscan.spec.ts
@@ -0,0 +1,21 @@
+import * as sinon from 'sinon'
+import { SinonSandbox } from 'sinon'
+import EtherscanHelper from '@helpers/etherscan'
+
+describe.only('Manual: Ethersca', () => {
+  let sandbox: SinonSandbox
+
+  beforeEach(() => {
+    sandbox = sinon.createSandbox()
+  })
+
+  afterEach(() => {
+    sandbox && sandbox.restore()
+  })
+
+  it('should fetchAllTransactions', async () => {
+    const daoFactoryAddress = '0xf96e6FD76BD0A15580604e1Ea5818D448b1041C0'
+    const response = await EtherscanHelper.fetchAllTransactions(daoFactoryAddress)
+    console.log(response) // eslint-disable-line no-console
+  })
+})
