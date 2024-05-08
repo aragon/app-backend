46940facaa5ce5098114d262e7b3e6fa2712c4c4
cristianizzo <cristianizzo@me.com>
2024-05-08 14:23:03 +0200

    unit test setup

---
+++ test/unit/services/indexer/pluginRepoLogs.spec.ts
@@ -0,0 +1,61 @@
+import * as sinon from 'sinon'
+import { SinonSandbox } from 'sinon'
+import { expect } from 'chai'
+import { PluginRepoLogs } from '@services/indexer/pluginRepoLogs'
+import logger from '@logger'
+import {NetworksEnum} from '@types'
+import { Models } from '@dbModels'
+import { UtilsIndexer } from '@models/utils/indexer'
+import Network from '@models/schema/network';
+
+describe('Indexer: PluginRepoLogs', () => {
+  let sandbox: SinonSandbox
+
+  beforeEach(async () => {
+    sandbox = sinon.createSandbox()
+  })
+
+  afterEach(async () => {
+    sandbox?.restore()
+  })
+
+  describe('start', () => {
+    it('should skip unsupported networks', async () => {
+      const networkFindStub = sandbox.stub(Models.Network, 'findByName').resolves(null)
+      const stubLogger = sandbox.stub(logger, 'verbose')
+      const crawlerStub = { crawl: sandbox.stub().resolves() }
+      await PluginRepoLogs.start()
+
+      expect(stubLogger.calledWith('Unsupported Network' as any)).to.be.true
+      expect(crawlerStub.crawl.notCalled).to.be.true
+      expect(networkFindStub.calledOnce).to.be.true
+    })
+
+    it('should process supported networks and run crawlers', async () => {
+      const networkFindStub = sandbox.stub(Models.Network, 'findByName').resolves({ lastBlockMetadataLog: 123 })
+      const crawlerStub = { crawl: sandbox.stub().resolves() }
+      const saveSyncStub = sandbox.stub(UtilsIndexer, 'saveSync').resolves()
+      sandbox.stub(PluginRepoLogs, 'createCrawler').returns(crawlerStub as any);
+      const loggerVerboseStub = sandbox.stub(logger, 'verbose')
+
+      await PluginRepoLogs.start()
+
+      expect(networkFindStub.callCount).to.eq(Object.values(Network.NETWORKS).length)
+      expect(crawlerStub.crawl.callCount).to.eq(Object.values(Network.NETWORKS).length)
+      expect(saveSyncStub.callCount).to.eq(Object.values(Network.NETWORKS).length)
+      expect(loggerVerboseStub.callCount).to.eq(Object.values(Network.NETWORKS).length + 1)
+      expect(loggerVerboseStub.calledWith('Start PluginRepoLogs' as any)).to.be.true
+      expect(loggerVerboseStub.calledWith('Finish PluginRepoLogs' as any)).to.be.true
+    })
+  })
+
+  it('processError', async () => {
+    const error = new Error('Test error')
+    const loggerStub = sinon.stub(logger, 'error')
+
+    await PluginRepoLogs.processError(error, NetworksEnum.mainnet)
+
+    expect(loggerStub.calledOnce).to.be.true
+    expect(loggerStub.calledWith('Error PluginRepoLogs' as any))
+  })
+})
