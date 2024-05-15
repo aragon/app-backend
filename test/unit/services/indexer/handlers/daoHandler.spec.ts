70ae07fc983383a596bba0f810561b93d5b59c0f
CryptoCrash <cristianizzo@me.com>
2024-05-15 18:58:00 +0200

    Dao Deposited & nativeTokenDeposited (#33)
    * deposit and native deposit
    
    * fix condition

--- test/unit/services/indexer/handlers/daoHandler.spec.ts
+++ test/unit/services/indexer/handlers/daoHandler.spec.ts
@@ -2,9 +2,10 @@ import * as sinon from 'sinon'
 import { SinonSandbox } from 'sinon'
 import { expect } from 'chai'
 import logger from '@logger'
-import { NetworksEnum } from '@types'
+import { ITransactionType, NetworksEnum } from '@types'
 import { beforeEach } from 'mocha'
 import { DaoHandler } from '@services/indexer/handlers/daoHandler'
+import { Models } from '@dbModels'
 
 describe('Indexer: DaoHandler', () => {
   let sandbox: SinonSandbox
@@ -26,14 +27,100 @@ describe('Indexer: DaoHandler', () => {
     expect(stubLogger.calledOnce).to.be.true
   })
 
-  it('deposited', async () => {
-    const event = { name: 'test' }
-    const txLog = { name: 'test' }
-    const network = NetworksEnum.mainnet
+  describe('deposited', () => {
+    it('should deposit native token', async () => {
+      const network = NetworksEnum.mainnet
+      const txLog = {
+        transactionHash: '0x123',
+        address: '0x456',
+        data: '0x789',
+        topics: ['0xabc'],
+        blockNumber: 1,
+      }
+      const fakeEvent = {
+        args: {
+          sender: '0x123',
+          amount: 10n,
+          _reference: 'some reference',
+        },
+      }
 
-    const stubLogger = sandbox.stub(logger, 'verbose')
-    await DaoHandler.deposited(event as any, txLog, network)
-    expect(stubLogger.calledOnce).to.be.true
+      const findTxHashSpy = sandbox.spy(Models.LogTransaction, 'findExistingLog')
+      const stubLogger = sandbox.stub(logger, 'verbose')
+
+      await DaoHandler.deposited(fakeEvent as any, txLog, network)
+
+      expect(findTxHashSpy.calledOnce).to.be.true
+      expect(findTxHashSpy.calledWith(txLog.transactionHash, ITransactionType.deposit, 0)).to.be.true
+      expect(stubLogger.calledTwice).to.be.true
+
+      const savedDaoLog = await Models.LogTransaction.findExistingLog(
+        txLog.transactionHash,
+        ITransactionType.deposit,
+        0,
+      )
+      expect(!!savedDaoLog).to.be.true
+
+      expect(savedDaoLog.entityId).to.exist
+      expect(savedDaoLog.blockNumber).to.eq(txLog.blockNumber)
+      expect(savedDaoLog.transactionHash).to.eq(txLog.transactionHash)
+      expect(savedDaoLog.network).to.eq(network)
+      expect(savedDaoLog.type).to.eq(ITransactionType.deposit)
+      expect(savedDaoLog.from).to.eq(fakeEvent.args.sender)
+      expect(savedDaoLog.to).to.eq(txLog.address)
+      expect(savedDaoLog.amount).to.eq(Number(fakeEvent.args.amount))
+      expect(savedDaoLog.tokenAddress).to.eq(null)
+      expect(savedDaoLog.tokenId).to.eq(null)
+      expect(savedDaoLog.reference).to.eq(fakeEvent.args._reference)
+      expect(savedDaoLog.actionIndex).to.eq(0)
+    })
+
+    it('should deposit erc20 token', async () => {
+      const network = NetworksEnum.mainnet
+      const txLog = {
+        transactionHash: '0x123',
+        address: '0x456',
+        data: '0x789',
+        topics: ['0xabc'],
+        blockNumber: 1,
+      }
+      const fakeEvent = {
+        args: {
+          sender: '0x123',
+          amount: 0n,
+          token: '0x0',
+        },
+      }
+
+      const findTxHashSpy = sandbox.spy(Models.LogTransaction, 'findExistingLog')
+      const stubLogger = sandbox.stub(logger, 'verbose')
+
+      await DaoHandler.deposited(fakeEvent as any, txLog, network)
+
+      expect(findTxHashSpy.calledOnce).to.be.true
+      expect(findTxHashSpy.calledWith(txLog.transactionHash, ITransactionType.deposit, 0)).to.be.true
+      expect(stubLogger.calledTwice).to.be.true
+
+      const savedDaoLog = await Models.LogTransaction.findExistingLog(
+        txLog.transactionHash,
+        ITransactionType.deposit,
+        0,
+      )
+      expect(!!savedDaoLog).to.be.true
+
+      expect(savedDaoLog.entityId).to.exist
+      expect(savedDaoLog.blockNumber).to.eq(txLog.blockNumber)
+      expect(savedDaoLog.transactionHash).to.eq(txLog.transactionHash)
+      expect(savedDaoLog.network).to.eq(network)
+      expect(savedDaoLog.type).to.eq(ITransactionType.deposit)
+      expect(savedDaoLog.from).to.eq(fakeEvent.args.sender)
+      expect(savedDaoLog.to).to.eq(txLog.address)
+      expect(savedDaoLog.amount).to.eq(Number(fakeEvent.args.amount))
+      expect(savedDaoLog.tokenAddress).to.eq(fakeEvent.args.token)
+      expect(savedDaoLog.tokenId).to.eq(null)
+      expect(savedDaoLog.reference).to.eq(null)
+      expect(savedDaoLog.actionIndex).to.eq(0)
+    })
   })
 
   it('executed', async () => {
@@ -57,13 +144,46 @@ describe('Indexer: DaoHandler', () => {
   })
 
   it('nativeTokenDeposited', async () => {
-    const event = { name: 'test' }
-    const txLog = { name: 'test' }
     const network = NetworksEnum.mainnet
+    const txLog = {
+      transactionHash: '0x123',
+      address: '0x456',
+      data: '0x789',
+      topics: ['0xabc'],
+      blockNumber: 1,
+    }
+    const fakeEvent = {
+      args: {
+        sender: '0x123',
+        amount: 10n,
+        _reference: 'some reference',
+      },
+    }
 
+    const findTxHashSpy = sandbox.spy(Models.LogTransaction, 'findExistingLog')
     const stubLogger = sandbox.stub(logger, 'verbose')
-    await DaoHandler.nativeTokenDeposited(event as any, txLog, network)
-    expect(stubLogger.calledOnce).to.be.true
+
+    await DaoHandler.nativeTokenDeposited(fakeEvent as any, txLog, network)
+
+    expect(findTxHashSpy.calledOnce).to.be.true
+    expect(findTxHashSpy.calledWith(txLog.transactionHash, ITransactionType.deposit, 0)).to.be.true
+    expect(stubLogger.calledTwice).to.be.true
+
+    const savedDaoLog = await Models.LogTransaction.findExistingLog(txLog.transactionHash, ITransactionType.deposit, 0)
+    expect(!!savedDaoLog).to.be.true
+
+    expect(savedDaoLog.entityId).to.exist
+    expect(savedDaoLog.blockNumber).to.eq(txLog.blockNumber)
+    expect(savedDaoLog.transactionHash).to.eq(txLog.transactionHash)
+    expect(savedDaoLog.network).to.eq(network)
+    expect(savedDaoLog.type).to.eq(ITransactionType.deposit)
+    expect(savedDaoLog.from).to.eq(fakeEvent.args.sender)
+    expect(savedDaoLog.to).to.eq(txLog.address)
+    expect(savedDaoLog.amount).to.eq(Number(fakeEvent.args.amount))
+    expect(savedDaoLog.tokenAddress).to.eq(null)
+    expect(savedDaoLog.tokenId).to.eq(null)
+    expect(savedDaoLog.reference).to.eq(null)
+    expect(savedDaoLog.actionIndex).to.eq(0)
   })
 
   it('newURI', async () => {
