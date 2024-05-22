bf51e745b800484d63d1bdb80872c73b56b371ba
cristianizzo <cristianizzo@me.com>
2024-05-22 03:07:02 +0200

    unit test

--- src/services/indexer/handlers/daoHandler.ts
+++ src/services/indexer/handlers/daoHandler.ts
@@ -1,16 +1,162 @@
 import logger from '@logger'
 import { ITransactionType, type NetworksEnum } from '@types'
-import { type LogDescription, ZeroAddress } from 'ethers'
+import { AbiCoder, type LogDescription, ZeroAddress } from 'ethers'
 import { Models } from '@dbModels'
 import DbTx from '@modules/dbTx'
 import { TransactionActionHandler } from '@services/indexer/handlers/transactionActionHandler'
 import Web3Helper from '@helpers/web3'
+import { ERC1155 } from '@artifacts/ERC1155'
 
 const llo = logger.logMeta.bind(null, { service: 'service:indexer:DaoHandler' })
 
 export const DaoHandler = {
   callbackReceived: async (parsedEvent: LogDescription, txLog: any, network: NetworksEnum) => {
-    logger.verbose('callbackReceived', llo({ txHash: txLog.transactionHash, network }))
+    const logInfo: any = {
+      txHash: txLog.transactionHash,
+      network,
+    }
+
+    const daoAddress = txLog.address
+    const calldata = '0x' + parsedEvent.args.data.slice(10)
+    const functionSig = parsedEvent.args.sig
+
+    switch (functionSig) {
+      case Web3Helper.onERC721Received: {
+        const decoded = Web3Helper.decodeCalldata(['address', 'address', 'uint256', 'uint8', 'uint8'], calldata)
+        if (!decoded) {
+          return
+        }
+
+        // TODO: handle multiple ERC721 transfer on same txs
+        //  onERC721Received will be trigger multiple times with different tokenId
+        const actionIndex = 0
+        const type = ITransactionType.deposit
+        const existingLog = await Models.LogTransaction.findExistingLog(txLog.transactionHash, type, actionIndex)
+
+        if (!existingLog) {
+          await DbTx.executeTxFn(async ({ session }) => {
+            const transaction: any = {
+              network,
+              blockNumber: txLog.blockNumber,
+              transactionHash: txLog.transactionHash,
+              from: decoded[1],
+              to: txLog.address, // dao address
+              daoAddress,
+              tokenAddress: parsedEvent.args.sender,
+              tokenId: decoded[2].toString(),
+              type: ITransactionType.deposit,
+            }
+
+            const logDb = await Models.LogTransaction.create(transaction, { session })
+            await session.commitTransaction()
+            await session.endSession()
+            logger.verbose('New ERC721 Deposit', llo({ logId: logDb.id, logInfo }))
+          })
+        }
+        break
+      }
+      case Web3Helper.onERC1155Received: {
+        const decoded = AbiCoder.defaultAbiCoder().decode(['address', 'address', 'uint256', 'uint8', 'uint8'], calldata)
+        if (!decoded) {
+          return
+        }
+
+        // TODO: handle multiple ERC721 transfer on same txs
+        //  onERC721Received will be trigger multiple times with different tokenId
+        const extraData = await Web3Helper.getDataFromTxReceipt({
+          txLog,
+          eventName: 'TransferSingle',
+          abi: ERC1155.abi,
+          network,
+        })
+        if (!extraData) {
+          return
+        }
+
+        const actionIndex = 0
+        const type = ITransactionType.deposit
+        const existingLog = await Models.LogTransaction.findExistingLog(txLog.transactionHash, type, actionIndex)
+
+        if (!existingLog) {
+          await DbTx.executeTxFn(async ({ session }) => {
+            const transaction: any = {
+              network,
+              blockNumber: txLog.blockNumber,
+              transactionHash: txLog.transactionHash,
+              daoAddress,
+              from:
+                extraData.events[0].parsed.args.from !== ZeroAddress
+                  ? extraData.events[0].parsed.args.from
+                  : extraData.events[0].parsed.args.operator,
+              to: extraData.events[0].parsed.args.to,
+              amount: Number(extraData.events[0].parsed.args.value),
+              tokenAddress: parsedEvent.args.sender,
+              tokenId: extraData.events[0].parsed.args.id?.toString(),
+              type: ITransactionType.deposit,
+            }
+
+            const logDb = await Models.LogTransaction.create(transaction, { session })
+            await session.commitTransaction()
+            await session.endSession()
+            logger.verbose('New ERC1155 Deposit', llo({ logId: logDb.id, logInfo }))
+          })
+        }
+        break
+      }
+      case Web3Helper.onERC1155BatchReceived: {
+        const decoded = AbiCoder.defaultAbiCoder().decode(
+          ['address', 'address', 'uint256[]', 'uint256[]', 'uint8'],
+          calldata,
+        )
+        if (!decoded) {
+          return
+        }
+
+        const extraData = await Web3Helper.getDataFromTxReceipt({
+          txLog,
+          eventName: 'TransferBatch',
+          abi: ERC1155.abi,
+          network,
+        })
+        if (!extraData) {
+          return
+        }
+
+        const actionIndex = 0
+        const type = ITransactionType.deposit
+        const existingLog = await Models.LogTransaction.findExistingLog(txLog.transactionHash, type, actionIndex)
+
+        if (!existingLog) {
+          await DbTx.executeTxFn(async ({ session }) => {
+            const transaction: any = {
+              network,
+              blockNumber: txLog.blockNumber,
+              transactionHash: txLog.transactionHash,
+              daoAddress,
+              from:
+                extraData.events[0].parsed.args.from !== ZeroAddress
+                  ? extraData.events[0].parsed.args.from
+                  : extraData.events[0].parsed.args.operator,
+              to: extraData.events[0].parsed.args.to,
+              tokenAddress: parsedEvent.args.sender,
+              tokenIds: extraData.events[0].parsed.args.ids?.map((w: bigint) => w.toString()),
+              amounts: extraData.events[0].parsed.args.values?.map((w: bigint) => Number(w)),
+              type: ITransactionType.deposit,
+            }
+
+            const logDb = await Models.LogTransaction.create(transaction, { session })
+            await session.commitTransaction()
+            await session.endSession()
+            logger.verbose('New ERC1155 Batch Deposit', llo({ logId: logDb.id, logInfo }))
+          })
+        }
+        break
+      }
+      default: {
+        logger.error('Unhandled functionSig', llo({ parsedEvent, txLog }))
+        break
+      }
+    }
   },
 
   deposited: async (parsedEvent: LogDescription, txLog: any, network: NetworksEnum) => {
@@ -48,7 +194,9 @@ export const DaoHandler = {
           const logDb = await Models.LogTransaction.create(transaction, { session })
           await session.commitTransaction()
           await session.endSession()
-          logger.verbose('New Deposit', llo({ logId: logDb.id, logInfo }))
+
+          const message = transaction.tokenAddress ? 'ERC20' : 'Native'
+          logger.verbose(`New ${message} Deposit`, llo({ logId: logDb.id, logInfo }))
         })
       }
     } catch (error) {
@@ -64,6 +212,7 @@ export const DaoHandler = {
 
     const actions = parsedEvent.args.actions
 
+    // TODO: if some proposal exchange some token to another token, its not handled here
     const handleAction = async (action: any, index: number) => {
       try {
         switch (true) {
