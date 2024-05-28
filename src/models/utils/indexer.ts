2cf0c356ead4d879841de99c1879ab56898202f7
cristianizzo <cristianizzo@me.com>
2024-05-29 00:08:47 +0200

    get and save token + unit test

--- src/models/utils/indexer.ts
+++ src/models/utils/indexer.ts
@@ -2,6 +2,14 @@ import type BlockchainLogCrawler from '@modules/blockchainLogCrawler'
 import type Network from '@models/schema/network'
 import DbTx from '@modules/dbTx'
 import type DBCrawler from '@models/utils/crawler'
+import { Models } from '@dbModels'
+import { type HexAddress, ITokenType, type NetworksEnum } from '@types'
+import TokenDetector from '@helpers/tokenDetector'
+import Web3Helper from '@helpers/web3'
+import logger from '@logger'
+import type Token from '@models/schema/token'
+
+const llo = logger.logMeta.bind(null, { service: 'models:utils:indexer' })
 
 export const UtilsIndexer = {
   saveSync: async (crawler: BlockchainLogCrawler, networkDb: Network, property: string) => {
@@ -32,5 +40,39 @@ export const UtilsIndexer = {
         await session.endSession()
       })
     }
+  },
+
+  saveAndGetToken: async (tokenAddress: HexAddress, network: NetworksEnum): Promise<null | Token> => {
+    const existingToken = await Models.Token.findByTokenAddressAndNetwork(tokenAddress, network)
+
+    if (existingToken) {
+      return existingToken
+    }
+
+    const tokenTypeInfo = await TokenDetector.detectTokenType(tokenAddress, network)
+
+    if (tokenTypeInfo?.type !== ITokenType.unknown) {
+      const tokenInfo = await Web3Helper.getTokenInfo(tokenAddress, network)
+
+      return await DbTx.executeTxFn(async ({ session }) => {
+        const rawToken = {
+          address: tokenAddress,
+          type: tokenTypeInfo?.type,
+          implementationAddress: tokenTypeInfo?.implementationAddress,
+          network,
+          name: tokenInfo.name,
+          decimals: tokenInfo.decimals,
+          symbol: tokenInfo.symbol,
+          totalSupply: tokenInfo.totalSupply,
+        }
+        const logDb = await Models.Token.create(rawToken, { session })
+        await session.commitTransaction()
+        await session.endSession()
+        logger.verbose('New Token', llo({ logId: logDb.id }))
+        return logDb
+      })
+    }
+
+    return null
   },
 }
