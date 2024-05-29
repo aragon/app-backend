f41c6412119619056e050568b2453bbf2a88aca1
cristianizzo <cristianizzo@me.com>
2024-05-29 16:52:47 +0200

    dune helper + unit test

--- src/helpers/dune.ts
+++ src/helpers/dune.ts
@@ -1,26 +1,26 @@
 import logger from '@logger'
 import axios from 'axios'
 import config from '@config'
-import { HexAddress } from '@types'
+import { type HexAddress } from '@types'
 
 const llo = logger.logMeta.bind(null, { service: 'helpers:DuneHelper' })
 
 interface WalletBalanceResponse {
-  request_time: string;
-  response_time: string;
-  wallet_address: string;
-  balances: Balance[];
+  request_time: string
+  response_time: string
+  wallet_address: string
+  balances: Balance[]
 }
 
 interface Balance {
-  chain: string;
-  chain_id: number;
-  address: HexAddress | string;
-  amount: string;
-  symbol?: string;
-  decimals?: number;
-  price_usd?: number;
-  value_usd?: number;
+  chain: string
+  chain_id: number
+  address: HexAddress | string
+  amount: string
+  symbol?: string
+  decimals?: number
+  price_usd?: number
+  value_usd?: number
 }
 
 // Dune only supports: base, polygon, ethereum, arbitrum
