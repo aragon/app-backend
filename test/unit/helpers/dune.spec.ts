edb8b5dc1da7128eff6db6bc6143ac9074bc5163
cristianizzo <cristianizzo@me.com>
2024-05-29 16:52:47 +0200

    dune helper + unit test

--- test/unit/helpers/dune.spec.ts
+++ test/unit/helpers/dune.spec.ts
@@ -2,7 +2,7 @@ import * as sinon from 'sinon'
 import { SinonSandbox } from 'sinon'
 import { expect } from 'chai'
 import DuneHelper from '@helpers/dune'
-import logger from "@logger";
+import logger from '@logger'
 
 describe('Helpers: Dune', () => {
   let sandbox: SinonSandbox
@@ -47,10 +47,22 @@ describe('Helpers: Dune', () => {
 
     it('should handle errors in API call', async () => {
       sandbox.stub(DuneHelper, 'axiosInstance').rejects(new Error('Network error'))
-      const loggerStub = sandbox.stub(logger, 'error');
+      const loggerStub = sandbox.stub(logger, 'error')
 
-      await expect(DuneHelper.getBalance('0x0eb63a3565942d16c1c1211bd78f1b3dcfe1a254')).to.be.rejectedWith(Error as any, 'Network error')
-      expect(loggerStub.calledOnce).to.be.true;
-    });
+      await expect(DuneHelper.getBalance('0x0eb63a3565942d16c1c1211bd78f1b3dcfe1a254')).to.be.rejectedWith(
+        Error as any,
+        'Network error',
+      )
+      expect(loggerStub.calledOnce).to.be.true
+    })
+  })
+
+  it('getBalance', async () => {
+    sandbox.stub(DuneHelper, 'axiosInstance').rejects(new Error('Network error'))
+    sandbox.stub(DuneHelper, '_rpCall').resolves({ data: 1 } as any)
+
+    const balance = await DuneHelper.getBalance('0x0eb63a3565942d16c1c1211bd78f1b3dcfe1a254')
+
+    expect(balance).to.eq(1)
   })
 })
