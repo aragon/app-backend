2be109a8997243353bac5087bbbd4894ef77f4f1
cristianizzo <cristianizzo@me.com>
2024-05-30 16:41:51 +0200

    experiment dune assets

---
+++ src/helpers/dune.ts
@@ -0,0 +1,62 @@
+import logger from '@logger'
+import axios from 'axios'
+import config from '@config'
+import { type HexAddress, type INetworks, NetworksEnum } from '@types'
+
+const llo = logger.logMeta.bind(null, { service: 'helpers:DuneHelper' })
+
+interface WalletBalanceResponse {
+  request_time: string
+  response_time: string
+  wallet_address: string
+  balances: Balance[]
+}
+
+interface Balance {
+  chain: string
+  chain_id: number
+  address: HexAddress | string
+  amount: string
+  symbol?: string
+  decimals?: number
+  price_usd?: number
+  value_usd?: number
+}
+
+// Dune only supports: base, polygon, ethereum, arbitrum
+const DuneHelper = {
+  axiosInstance: axios.create({
+    baseURL: config.DUNE.URI,
+    headers: { 'Content-Type': 'application/json', 'X-DUNE-API-KEY': config.DUNE.API_KEY },
+  }),
+
+  networksMap: {
+    ethereum: NetworksEnum.mainnet,
+    sepolia: NetworksEnum.sepolia,
+    polygon: NetworksEnum.polygon,
+    base: NetworksEnum.base,
+    arbitrum: NetworksEnum.arbitrum,
+  },
+
+  duneNetworkToAragon: (network: INetworks | string) => {
+    return DuneHelper.networksMap[network]
+  },
+
+  _rpCall: async (path: string) => {
+    try {
+      const url = `${path}`
+      const response = await DuneHelper.axiosInstance(url)
+      return response
+    } catch (error) {
+      logger.error('Error in DuneHelper RPC Call', llo({ error }))
+      throw error
+    }
+  },
+
+  getBalance: async (address: HexAddress): Promise<WalletBalanceResponse> => {
+    const resp = await DuneHelper._rpCall(`/beta/balance/${address}`)
+    return resp.data
+  },
+}
+
+export default DuneHelper
---
+++ src/services/indexer/aggregator/assetDune.ts
@@ -0,0 +1,73 @@
+import { AggregatorTypeEnum } from '@types'
+import DBCrawler from '@models/utils/crawler'
+import { ZeroAddress } from 'ethers'
+import { Models } from '@dbModels'
+import logger from '@logger'
+import DbTx from '@modules/dbTx'
+import DuneHelper from '@helpers/dune'
+import type Asset from '@models/schema/asset'
+import type Dao from '@models/schema/dao'
+import { UtilsIndexer } from '@models/utils/indexer'
+
+const llo = logger.logMeta.bind(null, { service: 'indexer:aggregator:AggregatorDuneAssets' })
+
+export const AggregatorDuneAssets = {
+  start: async () => {
+    logger.verbose('Start AggregatorDuneAssets', llo({}))
+
+    const aggregatorDb = await Models.Aggregator.findByType(AggregatorTypeEnum.assets)
+
+    const crawler = new DBCrawler({
+      model: Models.Dao,
+      onDocument: AggregatorDuneAssets.onDocument,
+      onError: (error: any) => {
+        logger.error('Error AggregatorDuneAssets', llo({ error }))
+      },
+      where: {},
+      batchSize: 500,
+      concurrency: 1,
+    })
+
+    await crawler.crawl()
+    await UtilsIndexer.saveAggregationSync(crawler, aggregatorDb, 'lastTimeSync')
+    logger.verbose('End AggregatorDuneAssets', llo({}))
+  },
+
+  onDocument: async (document: Dao) => {
+    const duneBalance = await DuneHelper.getBalance(document.daoAddress)
+
+    await Promise.all(
+      duneBalance.balances.map(async duneAsset => {
+        const existingAssetDb = await Models.Asset.findExistingLog(
+          document.daoAddress,
+          duneAsset.address === 'native' ? ZeroAddress : duneAsset.address,
+          DuneHelper.duneNetworkToAragon(duneAsset.chain),
+        )
+
+        const rawData: Partial<Asset> = {
+          amount: duneAsset.amount?.toString(),
+        }
+
+        if (!existingAssetDb) {
+          rawData.network = DuneHelper.duneNetworkToAragon(duneAsset.chain)
+          rawData.daoAddress = document.daoAddress
+          rawData.daoAddress = document.daoAddress
+          rawData.tokenAddress = duneAsset.address === 'native' ? ZeroAddress : duneAsset.address
+        }
+
+        await DbTx.executeTxFn(async ({ session }) => {
+          let logDb: any = null
+          if (existingAssetDb) {
+            logDb = await existingAssetDb.update(rawData, { session })
+          } else {
+            logDb = await Models.Asset.create(rawData, { session })
+          }
+
+          await session.commitTransaction()
+          await session.endSession()
+          logger.verbose(existingAssetDb ? 'Update Asset' : 'New Asset', llo({ logId: logDb?.id }))
+        })
+      }),
+    )
+  },
+}
