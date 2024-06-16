206a9764a7f95a72c28558a5a60b5cda8ffaaf52
cristianizzo <cristianizzo@me.com>
2024-06-16 21:45:24 +0200

    joi parse parmalink

--- src/services/aragon-api/routers/schema/dao.ts
+++ src/services/aragon-api/routers/schema/dao.ts
@@ -1,6 +1,7 @@
 import Joi from 'joi'
 import ValidationSchema from '@helpers/validationSchema'
 import { NetworksEnum } from '@types'
+import { getAddress } from 'ethers'
 
 const DaoSchema = {
   getWithPagination: Joi.object(
@@ -13,7 +14,26 @@ const DaoSchema = {
   ),
 
   getDaoByPermalink: Joi.object({
-    permalink: Joi.string().required(),
+    permalink: Joi.string()
+      .required()
+      .custom((value, helpers) => {
+        try {
+          const regex = /([a-z]+)-([0-9a-fA-Fx]+)/
+          const match = value.match(regex)
+
+          if (!match || match.length !== 3) {
+            return value
+          }
+
+          const [, network, address] = match
+          const checksumAddress = getAddress(address)
+
+          const formattedValue = `${network}-${checksumAddress}`
+          return formattedValue
+        } catch (error) {
+          return value
+        }
+      }, 'Permalink Address Validation'),
   }),
 
   getDaoPlugin: Joi.object({
