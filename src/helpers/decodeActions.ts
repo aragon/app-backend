8532b8a3662fe97a3f77e93a4805405819476fb0
Sishir Pokhrel <sisnet2010@gmail.com>
2024-06-26 17:37:00 +0545

    decodeAction fixes

--- src/helpers/decodeActions.ts
+++ src/helpers/decodeActions.ts
@@ -7,12 +7,14 @@ import { ERC20 } from '@artifacts/ERC20'
 import { ERC721 } from '@artifacts/ERC721'
 import { ERC1155 } from '@artifacts/ERC1155'
 import { DAOFactory } from '@artifacts/daoFactory'
+import { GovernanceERC20 } from '@artifacts/GovernanceERC20'
 
 const llo = logger.logMeta.bind(null, { service: 'DecodeActions' })
 
 interface Signature {
   method: string
   sig: string
+  fragment: FunctionFragment
 }
 
 class DecodeActions {
@@ -41,17 +43,17 @@ class DecodeActions {
     return null
   }
 
-
   getSignaturesFromAbi(abi: any[], name: string): Signature[] {
     return abi
-      .filter(item => item.type === 'function')
+      .filter(item => item.type === 'function' && item.stateMutability !== 'view' && item.stateMutability !== 'pure')
       .map(item => {
         try {
-          const fragment = FunctionFragment.from(item);
-          return { method: fragment.name, sig: fragment.format('minimal') };
+          const sig = FunctionFragment.getSelector(item.name, item.inputs)
+          const fragment = FunctionFragment.from(item)
+          return { method: item.name, sig, fragment }
         } catch (error) {
           logger.warn('Error creating FunctionFragment', llo({ error, item, name, abi }))
-          return null;
+          return null
         }
       })
       .filter((item): item is Signature => item !== null)
@@ -60,11 +62,15 @@ class DecodeActions {
   private setupSignatures() {
     const multisigSignatures: Signature[] = this.getSignaturesFromAbi(Multisig.abi, 'Multisig')
     const tokenVotingSignatures: Signature[] = this.getSignaturesFromAbi(MajorityVotingBase.abi, 'MajorityVotingBase')
-    const erc20MintableSignatures: Signature[] = this.getSignaturesFromAbi(IERC20MintableUpgradeable.abi, 'IERC20MintableUpgradeable')
+    const erc20MintableSignatures: Signature[] = this.getSignaturesFromAbi(
+      IERC20MintableUpgradeable.abi,
+      'IERC20MintableUpgradeable',
+    )
     const erc20Signatures: Signature[] = this.getSignaturesFromAbi(ERC20.abi, 'ERC20')
     const erc721Signatures: Signature[] = this.getSignaturesFromAbi(ERC721.abi, 'ERC721')
     const erc1155Signatures: Signature[] = this.getSignaturesFromAbi(ERC1155.abi, 'ERC1155')
     const daoFactorySignatures: Signature[] = this.getSignaturesFromAbi(DAOFactory.abi, 'DAOFactory')
+    const governanceSignatures: Signature[] = this.getSignaturesFromAbi(GovernanceERC20.abi, 'GovernanceERC20')
 
     this.allSignatures = [
       { name: 'DaoFactory', signatures: daoFactorySignatures, abi: DAOFactory.abi },
@@ -74,14 +80,14 @@ class DecodeActions {
       { name: 'ERC20', signatures: erc20Signatures, abi: ERC20.abi },
       { name: 'ERC721', signatures: erc721Signatures, abi: ERC721.abi },
       { name: 'ERC1155', signatures: erc1155Signatures, abi: ERC1155.abi },
+      { name: 'GovernanceERC20', signatures: governanceSignatures, abi: GovernanceERC20.abi },
     ]
   }
 
   private getFunctionFragment(dataHex: string, availableSignatures: Signature[]): FunctionFragment | undefined {
     const functionSelector = dataHex.substring(0, 10)
-    for (const { sig } of availableSignatures) {
-      const fragment = FunctionFragment.from(sig)
-      if (functionSelector === fragment.format('sighash')) {
+    for (const { sig, fragment } of availableSignatures) {
+      if (functionSelector === sig) {
         return fragment
       }
     }
