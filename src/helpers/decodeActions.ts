ffe6e92be7c76b6bf35934cc36c1953355ec5513
cristianizzo <cristianizzo@me.com>
2024-06-27 01:21:45 +0200

    DecodeActions unit test

--- src/helpers/decodeActions.ts
+++ src/helpers/decodeActions.ts
@@ -8,6 +8,9 @@ import { ERC721 } from '@artifacts/ERC721'
 import { ERC1155 } from '@artifacts/ERC1155'
 import { DAOFactory } from '@artifacts/daoFactory'
 import { GovernanceERC20 } from '@artifacts/GovernanceERC20'
+import FourByte from '@helpers/4byte'
+import Web3Helper from '@helpers/web3'
+import type Proposal from '@models/schema/proposal'
 
 const llo = logger.logMeta.bind(null, { service: 'DecodeActions' })
 
@@ -17,33 +20,97 @@ interface Signature {
   fragment: FunctionFragment
 }
 
+interface IDecodedData {
+  contractName?: string
+  functionName: any
+  decoded: any
+  textSignature: string
+}
+
 class DecodeActions {
-  allSignatures: { name: string; signatures: Signature[]; abi: any[] }[]
+  allSignatures: { contractName: string; signatures: Signature[]; abi: any[] }[]
   data: any
 
   constructor() {
     this.allSignatures = []
-    this.setupSignatures()
+    this._setupSignatures()
   }
 
-  public decodeData(data: string) {
+  public decodeTransfer(
+    action: { to: string; value: any; data: any },
+    document: Partial<Proposal>,
+  ): IDecodedData | null {
+    if (Web3Helper.isNativeTokenAction(action)) {
+      // native token transfer
+      return {
+        functionName: 'NativeTransfer',
+        textSignature: 'nativeTransfer(address,address,uint256)',
+        decoded: [document.daoAddress, action.to, action.value],
+      }
+    }
+
+    return null
+  }
+
+  public async decodeData(data: string): Promise<IDecodedData | null> {
+    let decoded = this._decodeWithAbi(data)
+
+    if (!decoded) {
+      decoded = await this._decodeFallback(data)
+    }
+
+    return decoded
+  }
+
+  _decodeWithAbi(data: string): IDecodedData | null {
     const dataHex = hexlify(data)
-    for (const { name, signatures, abi } of this.allSignatures) {
-      const fragment = this.getFunctionFragment(dataHex, signatures)
+    for (const { contractName, signatures, abi } of this.allSignatures) {
+      const fragment = this._getFunctionFragment(dataHex, signatures)
       if (fragment) {
         try {
           const iface = new Interface(abi)
           const decoded = iface.decodeFunctionData(fragment, dataHex)
-          return { name, fragment, decoded }
+
+          const functionName = fragment.name
+          const parameters = fragment.inputs.map((input: any) => input.type).join(',')
+          const textSignature = `${functionName}(${parameters})`
+
+          return { functionName, textSignature, decoded: decoded?.toArray(), contractName }
         } catch (error) {
-          logger.error('Error decoding data', llo({ error, name, fragment, dataHex }))
+          logger.error('Error decoding action data with abi', llo({ error, contractName, fragment, dataHex }))
         }
       }
     }
     return null
   }
 
-  getSignaturesFromAbi(abi: any[], name: string): Signature[] {
+  async _decodeFallback(data: string): Promise<IDecodedData | null> {
+    try {
+      const dataHex = hexlify(data)
+      const functionSelector = dataHex.substring(0, 10)
+      const response = await FourByte.getSignatures(functionSelector)
+
+      if (!response || response.count === 0) {
+        return null
+      }
+
+      const signatureInfo = response.results[response.results.length - 1]
+
+      const iface = new Interface([`function ${signatureInfo.text_signature}`])
+      const decoded = iface.decodeFunctionData(signatureInfo.text_signature, data as any)
+
+      return {
+        functionName: signatureInfo.text_signature.split('(')[0],
+        textSignature: signatureInfo.text_signature,
+        decoded: decoded?.toArray(),
+      }
+    } catch (error) {
+      logger.error('Error decoding action data', llo({ error, data }))
+      return null
+    }
+  }
+
+  _getSignaturesFromAbi(abi: any[], name: string): Signature[] {
     return abi
       .filter(item => item.type === 'function' && item.stateMutability !== 'view' && item.stateMutability !== 'pure')
       .map(item => {
@@ -59,32 +126,36 @@ class DecodeActions {
       .filter((item): item is Signature => item !== null)
   }
 
-  private setupSignatures() {
-    const multisigSignatures: Signature[] = this.getSignaturesFromAbi(Multisig.abi, 'Multisig')
-    const tokenVotingSignatures: Signature[] = this.getSignaturesFromAbi(MajorityVotingBase.abi, 'MajorityVotingBase')
-    const erc20MintableSignatures: Signature[] = this.getSignaturesFromAbi(
+  _setupSignatures() {
+    const multisigSignatures: Signature[] = this._getSignaturesFromAbi(Multisig.abi, 'Multisig')
+    const tokenVotingSignatures: Signature[] = this._getSignaturesFromAbi(MajorityVotingBase.abi, 'MajorityVotingBase')
+    const erc20MintableSignatures: Signature[] = this._getSignaturesFromAbi(
       IERC20MintableUpgradeable.abi,
       'IERC20MintableUpgradeable',
     )
-    const erc20Signatures: Signature[] = this.getSignaturesFromAbi(ERC20.abi, 'ERC20')
-    const erc721Signatures: Signature[] = this.getSignaturesFromAbi(ERC721.abi, 'ERC721')
-    const erc1155Signatures: Signature[] = this.getSignaturesFromAbi(ERC1155.abi, 'ERC1155')
-    const daoFactorySignatures: Signature[] = this.getSignaturesFromAbi(DAOFactory.abi, 'DAOFactory')
-    const governanceSignatures: Signature[] = this.getSignaturesFromAbi(GovernanceERC20.abi, 'GovernanceERC20')
+    const erc20Signatures: Signature[] = this._getSignaturesFromAbi(ERC20.abi, 'ERC20')
+    const erc721Signatures: Signature[] = this._getSignaturesFromAbi(ERC721.abi, 'ERC721')
+    const erc1155Signatures: Signature[] = this._getSignaturesFromAbi(ERC1155.abi, 'ERC1155')
+    const daoFactorySignatures: Signature[] = this._getSignaturesFromAbi(DAOFactory.abi, 'DAOFactory')
+    const governanceSignatures: Signature[] = this._getSignaturesFromAbi(GovernanceERC20.abi, 'GovernanceERC20')
 
     this.allSignatures = [
-      { name: 'DaoFactory', signatures: daoFactorySignatures, abi: DAOFactory.abi },
-      { name: 'Multisig', signatures: multisigSignatures, abi: Multisig.abi },
-      { name: 'MajorityVotingBase', signatures: tokenVotingSignatures, abi: MajorityVotingBase.abi },
-      { name: 'IERC20MintableUpgradeable', signatures: erc20MintableSignatures, abi: IERC20MintableUpgradeable.abi },
-      { name: 'ERC20', signatures: erc20Signatures, abi: ERC20.abi },
-      { name: 'ERC721', signatures: erc721Signatures, abi: ERC721.abi },
-      { name: 'ERC1155', signatures: erc1155Signatures, abi: ERC1155.abi },
-      { name: 'GovernanceERC20', signatures: governanceSignatures, abi: GovernanceERC20.abi },
+      { contractName: 'DaoFactory', signatures: daoFactorySignatures, abi: DAOFactory.abi },
+      { contractName: 'Multisig', signatures: multisigSignatures, abi: Multisig.abi },
+      { contractName: 'MajorityVotingBase', signatures: tokenVotingSignatures, abi: MajorityVotingBase.abi },
+      {
+        contractName: 'IERC20MintableUpgradeable',
+        signatures: erc20MintableSignatures,
+        abi: IERC20MintableUpgradeable.abi,
+      },
+      { contractName: 'ERC20', signatures: erc20Signatures, abi: ERC20.abi },
+      { contractName: 'ERC721', signatures: erc721Signatures, abi: ERC721.abi },
+      { contractName: 'ERC1155', signatures: erc1155Signatures, abi: ERC1155.abi },
+      { contractName: 'GovernanceERC20', signatures: governanceSignatures, abi: GovernanceERC20.abi },
     ]
   }
 
-  private getFunctionFragment(dataHex: string, availableSignatures: Signature[]): FunctionFragment | undefined {
+  _getFunctionFragment(dataHex: string, availableSignatures: Signature[]): FunctionFragment | undefined {
     const functionSelector = dataHex.substring(0, 10)
     for (const { sig, fragment } of availableSignatures) {
       if (functionSelector === sig) {
--- test/unit/helpers/decodeActions.spec.ts
+++ test/unit/helpers/decodeActions.spec.ts
@@ -2,6 +2,9 @@ import * as sinon from 'sinon'
 import { SinonSandbox } from 'sinon'
 import DecodeActions from '@helpers/decodeActions'
 import { expect } from 'chai'
+import { Fragment, FunctionFragment } from 'ethers'
+import FourByte from '@helpers/4byte'
+import Logger from '@logger'
 
 describe('Helpers: DecodeActions', () => {
   let sandbox: SinonSandbox
@@ -24,9 +27,369 @@ describe('Helpers: DecodeActions', () => {
         data: '0x40c10f19000000000000000000000000284803c34a3f049f787e2562e6f8c084bdbc31970000000000000000000000000000000000000000000000000de0b6b3a7640000',
       }
 
-      const result = decodeActions.decodeData(action.data)
+      const spyDecodeAbi = sandbox.spy(decodeActions, '_decodeWithAbi')
+      const spyDecodeFallback = sandbox.spy(decodeActions, '_decodeFallback')
+
+      const result = await decodeActions.decodeData(action.data)
+
       const toAddress = result?.decoded[0].toLowerCase()
+      expect(spyDecodeAbi.calledOnce).to.be.true
+      expect(spyDecodeFallback.notCalled).to.be.true
       expect(toAddress).to.be.equal('0x284803c34a3f049f787e2562e6f8c084bdbc3197')
+    })
+
+    it('Should decodeData with fallback', async () => {
+      const decodeActions = new DecodeActions()
+
+      const action = {
+        to: '0x8e1e51BdeA4Ea2C42FF2d0f7D3303D417603298F',
+        value: '0',
+        data: '0x3628731c00000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000001000000000000000000000000ef32dc2b02bfa082f11aa6f57154f4079ffe9bbc',
+      }
+
+      const stubDecodeAbi = sandbox.stub(decodeActions, '_decodeWithAbi').returns(null)
+      const stubDecodeFallback = sandbox.stub(decodeActions, '_decodeFallback').resolves(true as any)
+
+      const result = await decodeActions.decodeData(action.data)
+
+      expect(result).to.be.true
+      expect(stubDecodeAbi.calledOnceWith(action.data)).to.be.true
+      expect(stubDecodeFallback.calledOnceWith(action.data)).to.be.true
+    })
+
+    it('Should fail decodeData', async () => {
+      const decodeActions = new DecodeActions()
+
+      const action = {
+        to: '0x3949F15155D4b85d0159aB79cbf38DC51c41DD9F',
+        value: '0',
+        data: '0x00e10f10000000000000000000000000284803c34a3f049f787e2562e6f8c084bdbc31970000000000000000000000000000000000000000000000000de0b6b3a7640000',
+      }
+
+      const spyDecodeAbi = sandbox.spy(decodeActions, '_decodeWithAbi')
+      const spyDecodeFallback = sandbox.spy(decodeActions, '_decodeFallback')
+
+      const result = await decodeActions.decodeData(action.data)
+      expect(result?.decoded).to.be.undefined
+      expect(spyDecodeAbi.calledOnce).to.be.true
+      expect(spyDecodeFallback.calledOnce).to.be.true
+    })
+  })
+
+  describe('_decodeWithAbi', () => {
+    it('should decode data using the provided ABI', () => {
+      const decodeActions = new DecodeActions()
+      const data =
+        '0x40c10f19000000000000000000000000284803c34a3f049f787e2562e6f8c084bdbc31970000000000000000000000000000000000000000000000000de0b6b3a7640000'
+
+      const abi = [
+        {
+          name: 'mint',
+          type: 'function',
+          inputs: [
+            { name: 'to', type: 'address' },
+            { name: 'amount', type: 'uint256' },
+          ],
+        },
+      ]
+      const functionFragment = Fragment.from(abi[0])
+
+      decodeActions.allSignatures = [
+        {
+          contractName: 'IERC20MintableUpgradeable',
+          signatures: [{ method: 'mint', sig: '0x40c10f19', fragment: functionFragment as any }],
+          abi: abi,
+        },
+      ]
+
+      const result = decodeActions._decodeWithAbi(data)
+
+      expect(result).to.deep.equal({
+        contractName: 'IERC20MintableUpgradeable',
+        functionName: 'mint',
+        textSignature: 'mint(address,uint256)',
+        decoded: ['0x284803C34A3F049f787E2562e6F8C084bdBC3197', 1000000000000000000n],
+      })
+    })
+
+    it('should return null if no matching ABI is found', () => {
+      const decodeActions = new DecodeActions()
+      const data =
+        '0x40c10f19000000000000000000000000284803c34a3f049f787e2562e6f8c084bdbc31970000000000000000000000000000000000000000000000000de0b6b3a7640000'
+
+      // No ABI setup
+      decodeActions.allSignatures = []
+
+      const result = decodeActions._decodeWithAbi(data)
+      expect(result).to.be.null
+    })
+
+    it('should return null if decoding fails', () => {
+      const decodeActions = new DecodeActions()
+      const data = '0x40c10f19000000000000000000000000' // Invalid data
+
+      const abi = [
+        {
+          name: 'mint',
+          type: 'function',
+          inputs: [
+            { name: 'to', type: 'address' },
+            { name: 'amount', type: 'uint256' },
+          ],
+        },
+      ]
+      const functionFragment = Fragment.from(abi[0])
+
+      decodeActions.allSignatures = [
+        {
+          contractName: 'IERC20MintableUpgradeable',
+          signatures: [{ method: 'mint', sig: '0x40c10f19', fragment: functionFragment as any }],
+          abi: abi,
+        },
+      ]
+
+      const stubLogger = sandbox.stub(Logger, 'error')
+      const result = decodeActions._decodeWithAbi(data)
+      expect(result).to.be.null
+      expect(stubLogger.calledWith('Error decoding action data with abi' as any)).to.be.true
+    })
+  })
+
+  describe('_decodeFallback', () => {
+    it('should decode data using FourByte fallback', async () => {
+      const decodeActions = new DecodeActions()
+      const data =
+        '0xee57e36f00000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000035697066733a2f2f516d4e753239435378354276596a506a786d716e6a6a6d5a68326e6a4e4b6e68346a7a566b5a6d476d47786674580000000000000000000000'
+
+      const stubFourByte = sandbox.stub(FourByte, 'getSignatures').resolves({
+        count: 1,
+        next: null,
+        previous: null,
+        results: [
+          {
+            id: 513956,
+            created_at: '2022-03-03T07:59:06.062305Z',
+            text_signature: 'setMetadata(bytes)',
+            hex_signature: '0xee57e36f',
+            bytes_signature: 'îWão',
+          },
+        ],
+      })
+
+      const result = await decodeActions._decodeFallback(data)
+      expect(result).to.deep.equal({
+        functionName: 'setMetadata',
+        textSignature: 'setMetadata(bytes)',
+        decoded: [
+          '0x697066733a2f2f516d4e753239435378354276596a506a786d716e6a6a6d5a68326e6a4e4b6e68346a7a566b5a6d476d4778667458',
+        ],
+      })
+      expect(stubFourByte.calledOnce).to.be.true
+    })
+
+    it('should return null if fail to decode', async () => {
+      const decodeActions = new DecodeActions()
+      const data = '0xee57e36f0000000000000000000000000000000000000000000000000000000000000001'
+
+      const stubFourByte = sandbox.stub(FourByte, 'getSignatures').resolves({
+        count: 1,
+        next: null,
+        previous: null,
+        results: [
+          {
+            id: 513956,
+            created_at: '2022-03-03T07:59:06.062305Z',
+            text_signature: 'setMetadata(bytes)',
+            hex_signature: '0xee57e36f',
+            bytes_signature: 'îWão',
+          },
+        ],
+      })
+
+      const result = await decodeActions._decodeFallback(data)
+      expect(result).to.be.null
+      expect(stubFourByte.calledOnce).to.be.true
+    })
+
+    it('should return null if no signatures are found', async () => {
+      const decodeActions = new DecodeActions()
+      const data = '0x1234567800000000000000000000000000000000000000000000000000000000'
+
+      const stubFourByte = sandbox.stub(FourByte, 'getSignatures').resolves({
+        count: 0,
+        next: null,
+        previous: null,
+        results: [],
+      })
+
+      const result = await decodeActions._decodeFallback(data)
+      expect(result).to.be.null
+      expect(stubFourByte.calledOnce).to.be.true
+    })
+
+    it('should return null if fail to getSignatures', async () => {
+      const decodeActions = new DecodeActions()
+      const data = '0xee57e36f0000000000000000000000000000000000000000000000000000000000000001'
+
+      const stubLogger = sandbox.stub(Logger, 'error')
+      const stubFourByte = sandbox.stub(FourByte, 'getSignatures').rejects(new Error('fake-error'))
+
+      const result = await decodeActions._decodeFallback(data)
+      expect(result).to.be.null
+      expect(stubFourByte.calledOnce).to.be.true
+      expect(stubLogger.calledOnceWith('Error decoding action data' as any)).to.be.true
+    })
+  })
+
+  describe('_getSignaturesFromAbi', () => {
+    it('should correctly extract signatures from ABI', () => {
+      const decodeActions = new DecodeActions()
+      const abi = [
+        {
+          name: 'transfer',
+          type: 'function',
+          stateMutability: 'nonpayable',
+          inputs: [
+            {
+              name: 'recipient',
+              type: 'address',
+            },
+            {
+              name: 'amount',
+              type: 'uint256',
+            },
+          ],
+        },
+        {
+          name: 'approve',
+          type: 'function',
+          stateMutability: 'nonpayable',
+          inputs: [
+            {
+              name: 'spender',
+              type: 'address',
+            },
+            {
+              name: 'amount',
+              type: 'uint256',
+            },
+          ],
+        },
+      ]
+
+      const name = 'ERC20'
+      const signatures = decodeActions._getSignaturesFromAbi(abi, name)
+
+      expect(signatures.length).to.eq(2)
+      expect(signatures[0].method).to.eq('transfer')
+      expect(signatures[1].method).to.eq('approve')
+    })
+
+    it('should skip view and pure functions', () => {
+      const decodeActions = new DecodeActions()
+      const abi = [
+        {
+          name: 'balanceOf',
+          type: 'function',
+          stateMutability: 'view',
+          inputs: [
+            {
+              name: 'account',
+              type: 'address',
+            },
+          ],
+        },
+        {
+          name: 'totalSupply',
+          type: 'function',
+          stateMutability: 'pure',
+          inputs: [],
+        },
+      ]
+
+      const name = 'ERC20'
+      const signatures = decodeActions._getSignaturesFromAbi(abi, name)
+
+      expect(signatures.length).to.eq(0)
+    })
+
+    it('should handle invalid type', () => {
+      const decodeActions = new DecodeActions()
+      const abi = [
+        {
+          name: 'invalidFunction',
+          type: 'function',
+          stateMutability: 'nonpayable',
+          inputs: [
+            {
+              name: 'param',
+              type: 'invalidType',
+            },
+          ],
+        },
+      ]
+
+      const name = 'InvalidContract'
+      const signatures = decodeActions._getSignaturesFromAbi(abi, name)
+
+      expect(signatures.length).to.eq(0)
+    })
+
+    it('should handle error', () => {
+      const decodeActions = new DecodeActions()
+      const abi = [
+        {
+          name: 'invalidFunction',
+          type: 'function',
+          stateMutability: 'nonpayable',
+        },
+      ]
+
+      const name = 'InvalidContract'
+      sandbox.stub(FunctionFragment, 'getSelector').throws(new Error('fake-error'))
+      const stubLogger = sandbox.stub(Logger, 'warn')
+      const signatures = decodeActions._getSignaturesFromAbi(abi, name)
+
+      expect(signatures.length).to.eq(0)
+      expect(stubLogger.calledWith('Error creating FunctionFragment' as any)).to.be.true
+    })
+  })
+
+  describe('_setupSignatures', () => {
+    it('should set up signatures correctly', () => {
+      const decodeActions = new DecodeActions()
+
+      const allSignatures = decodeActions.allSignatures.map(({ contractName, abi }) => ({ contractName, abi }))
+      expect(allSignatures.length).to.eq(8)
+      expect(allSignatures[0].contractName).to.eq('DaoFactory')
+      expect(allSignatures[1].contractName).to.eq('Multisig')
+      expect(allSignatures[2].contractName).to.eq('MajorityVotingBase')
+      expect(allSignatures[3].contractName).to.eq('IERC20MintableUpgradeable')
+      expect(allSignatures[4].contractName).to.eq('ERC20')
+      expect(allSignatures[5].contractName).to.eq('ERC721')
+      expect(allSignatures[6].contractName).to.eq('ERC1155')
+      expect(allSignatures[7].contractName).to.eq('GovernanceERC20')
+    })
+  })
+
+  describe('_getFunctionFragment', () => {
+    it('should return the correct function fragment for a valid function selector', () => {
+      const decodeActions = new DecodeActions()
+      const dataHex = '0x095ea7b3000000000000000000000000' // Example function selector with data
+
+      const availableSignatures = { method: 'approve', sig: '0x095ea7b3', fragment: 'test' as any }
+
+      const fragment = decodeActions._getFunctionFragment(dataHex, [availableSignatures])
+      expect(fragment).to.deep.equal('test')
+    })
+
+    it('should return undefined for an invalid function selector', () => {
+      const decodeActions = new DecodeActions()
+      const dataHex = '0x12345678000000000000000000000000' // Invalid function selector
+      const availableSignatures = { method: 'approve', sig: '0x095ea7b3', fragment: 'test' as any }
+
+      const fragment = decodeActions._getFunctionFragment(dataHex, [availableSignatures])
+      expect(fragment).to.be.undefined
     })
   })
 })
