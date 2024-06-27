9d789e10e89c48a66761155519173ca55e0d6409
CryptoCrash <cristianizzo@me.com>
2024-06-27 14:20:59 +0200

    Feat/app 3341 (#86)
    * dao subdomain
    
    * dao subdomain
    
    * dao subdomain
    
    * member store all delegateChanged events

--- src/services/aragon-indexer/handlers/memberHandler.ts
+++ src/services/aragon-indexer/handlers/memberHandler.ts
@@ -111,43 +111,40 @@ export const MemberHandler = {
         GovernanceERC20.abi,
       )
 
-      const delegationVote = delegationVotesChangedLogs.find(
-        (log: any) => Web3Helper.formatAddress(log.txLog.topics[1]) === parsedEvent.args.toDelegate,
+      await Promise.all(
+        delegationVotesChangedLogs.map(async (delegationVoteLog: any) => {
+          const memberAddress = Web3Helper.formatAddress(delegationVoteLog.txLog.topics[1])
+
+          await DbTx.executeTxFn(async ({ session }) => {
+            const rawDaoMember = {
+              transactionHash: info.transactionHash,
+              blockNumber: info.blockNumber,
+              network: info.network,
+              address: memberAddress,
+              event: parsedEvent.name,
+              tokenAddress: info.address,
+              fromDelegate: parsedEvent.args.fromDelegate,
+              toDelegate: parsedEvent.args.toDelegate,
+              delegatingMember: parsedEvent.args.delegator,
+              previousVotingPower: delegationVoteLog?.parsed!.args.previousBalance.toString(),
+              newVotingPower: delegationVoteLog?.parsed!.args.newBalance.toString(),
+              pluginAddress: relatedPlugin.pluginAddress,
+            }
+
+            const daoMember = await Models.LogMember.create(rawDaoMember, { session } as any)
+            await session.commitTransaction()
+            await session.endSession()
+
+            logger.verbose(
+              'New LogMembers Delegation Changed',
+              llo({
+                ...info,
+                logId: daoMember.id,
+              }),
+            )
+          })
+        }),
       )
-
-      if (!delegationVote) {
-        logger.warn('DelegateVotesChanged not found. Invalid log', llo(info))
-        return
-      }
-
-      await DbTx.executeTxFn(async ({ session }) => {
-        const rawDaoMember = {
-          transactionHash: info.transactionHash,
-          blockNumber: info.blockNumber,
-          network: info.network,
-          address: parsedEvent.args.toDelegate,
-          event: parsedEvent.name,
-          tokenAddress: info.address,
-          fromDelegate: parsedEvent.args.fromDelegate,
-          toDelegate: parsedEvent.args.toDelegate,
-          delegatingMember: parsedEvent.args.delegator,
-          previousVotingPower: delegationVote?.parsed!.args.previousBalance.toString(),
-          newVotingPower: delegationVote?.parsed!.args.newBalance.toString(),
-          pluginAddress: relatedPlugin.pluginAddress,
-        }
-
-        const daoMember = await Models.LogMember.create(rawDaoMember, { session } as any)
-        await session.commitTransaction()
-        await session.endSession()
-
-        logger.verbose(
-          'New LogMembers Delegation Changed',
-          llo({
-            ...info,
-            logId: daoMember.id,
-          }),
-        )
-      })
     }
   },
 }
