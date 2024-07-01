571073e29dac58601a122bdf154200639fc556cc
CryptoCrash <cristianizzo@me.com>
2024-07-01 21:26:24 +0200

    app config (#90)

--- runners/initialData.ts
+++ runners/initialData.ts
@@ -1,4 +1,7 @@
+import * as dotenv from 'dotenv'
+
 import Runner from '@modules/runner'
 import InitialData from '@src/../initialData/index'
+dotenv.config({ path: '../.env.aragon-api' })
 
 Runner([{ app: InitialData }])
