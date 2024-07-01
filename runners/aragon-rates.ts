3a04cbc02caa9e20bb1b23dd03a841d21671b02e
cristianizzo <cristianizzo@me.com>
2024-07-01 21:17:34 +0200

    app config

--- runners/aragon-rates.ts
+++ runners/aragon-rates.ts
@@ -1,4 +1,7 @@
+import * as dotenv from 'dotenv'
+
 import Runner from '@modules/runner'
 import AragonRatesService from '@services/aragon-rates'
+dotenv.config({ path: '../.env.aragon-rates' })
 
 Runner([{ app: AragonRatesService }])
