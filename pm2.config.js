ba753e07b929d705b70c4bf4530d1ea4807659c9
cristianizzo <cristianizzo@me.com>
2024-07-01 23:21:46 +0200

    pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -14,5 +14,27 @@ module.exports = {
         ...dotenv.config({ path: path.resolve(__dirname, '.env.aragon-api') }).parsed,
       },
     },
+    {
+      name: 'aragon-indexer',
+      cwd: path.resolve(__dirname, ''),
+      script: 'yarn',
+      args: 'service:aragon-indexer',
+      autorestart: true,
+      env: {
+        INSTANCE_ID: 'aragon-indexer',
+        ...dotenv.config({ path: path.resolve(__dirname, '.env.aragon-indexer') }).parsed,
+      },
+    },
+    {
+      name: 'aragon-rates',
+      cwd: path.resolve(__dirname, ''),
+      script: 'yarn',
+      args: 'service:aragon-rates',
+      autorestart: true,
+      env: {
+        INSTANCE_ID: 'aragon-rates',
+        ...dotenv.config({ path: path.resolve(__dirname, '.env.aragon-rates') }).parsed,
+      },
+    },
   ],
 }
