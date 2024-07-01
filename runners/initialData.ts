25c67011f3f1585becf5fa6601851ae206b6fc50
cristianizzo <cristianizzo@me.com>
2024-07-01 23:14:17 +0200

    app config

--- runners/initialData.ts
+++ runners/initialData.ts
@@ -2,6 +2,6 @@ import { loadConfig } from '../config/environment'
 
 import Runner from '@modules/runner'
 import InitialData from '@src/../initialData/index'
-loadConfig('.env.aragon-api')
+loadConfig('../.env.aragon-api')
 
 Runner([{ app: InitialData }])
