d423bf27fa577c8fe654963708357d83cd0b8afe
cristianizzo <cristianizzo@me.com>
2024-07-03 15:31:58 +0200

    fix duplicate member database

--- src/services/aragon-indexer/aggregator/plugin.ts
+++ src/services/aragon-indexer/aggregator/plugin.ts
@@ -5,6 +5,7 @@ import DbTx from '@modules/dbTx'
 import type Plugin from '@models/schema/plugin'
 import { NetworkHelper } from '@helpers/network'
 import { type NetworksEnum } from '@types'
+import ProxyContractHelper from "@helpers/proxyContract";
 
 const llo = logger.logMeta.bind(null, { service: 'indexer:aggregator:AggregatorPlugin' })
 
@@ -39,6 +40,10 @@ export const AggregatorPlugin = {
     await DbTx.executeTxFn(async ({ session }) => {
       let logDb: any
       if (!existingLog) {
+        const implementationAddress = await ProxyContractHelper.getImplementationAddress(document.address!, document.network!)
+        if (implementationAddress) {
+          document.implementationAddress = implementationAddress
+        }
         logDb = await Models.Plugin.create(document as any, { session } as any)
       } else {
         logDb = await existingLog.update(document, { session })
