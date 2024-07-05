9ab2fb95468dd17c90437a6eda4452f373e5a894
ccir105 <sisnet2010@gmail.com>
2024-07-05 16:52:39 +0545

    During aggregator project pipeline check if subdomain is empty then return null   (#95)
    * aggregator
    
    * fix plugin subdomain in proposal and settings
    
    ---------
    
    Co-authored-by: cristianizzo <cristianizzo@me.com>

--- src/services/aragon-indexer/aggregator/setting.ts
+++ src/services/aragon-indexer/aggregator/setting.ts
@@ -72,6 +72,10 @@ export const AggregatorSetting = {
           from: 'logPluginSetupProcessor',
           localField: '_id.pluginAddress',
           foreignField: 'pluginAddress',
+          pipeline: [
+            { $match: { event: 'InstallationPrepared' } },
+            { $project: { daoAddress: 1, pluginAddress: 1, pluginSetupRepo: 1, subdomain: 1, tokenAddress: 1 } },
+          ],
           as: 'pluginInfo',
         },
       },
