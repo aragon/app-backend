81310d4ed1bd870bb1f98f3813f189d4bbcca209
cristianizzo <cristianizzo@me.com>
2024-07-09 20:06:06 +0200

    endpointa pairData

---
+++ src/modules/pairData.ts
@@ -0,0 +1,48 @@
+import { Models } from '@dbModels'
+import { type HexAddress, type IPaginationParams, type IPairParams, type NetworksEnum } from '@types'
+
+const PairDataModule = {
+  pairFromPaginationParams: async (paginationParams: IPaginationParams): Promise<IPaginationParams> => {
+    // resolve ens from search
+    if (paginationParams?.search && paginationParams?.search?.length > 0) {
+      const searchStr = paginationParams.search
+      const ethRegex = /\.eth$/
+
+      if (ethRegex.test(searchStr)) {
+        const member = await Models.Member.findByEns(searchStr as any)
+        if (member) {
+          paginationParams.search = member.address
+        }
+      }
+    }
+
+    return paginationParams
+  },
+
+  pairFromExtraParams: async <
+    T extends { network?: NetworksEnum; daoAddress?: HexAddress; memberAddress?: HexAddress },
+  >(
+    extraParams: T,
+    pairParams: IPairParams,
+  ): Promise<T> => {
+    // resolve daoId
+    if (pairParams?.daoId) {
+      const daoDb = await Models.Dao.findByEntityId(pairParams.daoId)
+      if (daoDb) {
+        extraParams.network = daoDb.network
+        extraParams.daoAddress = daoDb.address
+      }
+    }
+
+    // resolve ens
+    if (pairParams?.ens) {
+      const memberDb = await Models.Member.findByEns(pairParams.ens as any)
+      if (memberDb) {
+        extraParams.memberAddress = memberDb.address
+      }
+    }
+
+    return extraParams
+  },
+}
+export default PairDataModule
