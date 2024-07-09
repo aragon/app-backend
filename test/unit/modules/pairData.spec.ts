3ff5c348b75dc41f7d51bf0f982970d2cd750ceb
cristianizzo <cristianizzo@me.com>
2024-07-09 20:06:06 +0200

    endpointa pairData

---
+++ test/unit/modules/pairData.spec.ts
@@ -0,0 +1,144 @@
+import sinon from 'sinon'
+import { expect } from 'chai'
+import { NetworksEnum } from '@types'
+import PairDataModule from '@modules/pairData'
+import Member from '@models/schema/member'
+import { Models } from '@dbModels'
+import Dao from '@models/schema/dao'
+
+describe('Modules:PairDataModule', () => {
+  let sandbox: sinon.SinonSandbox
+  let rawMember: Partial<Member>
+  let rawDao: Partial<Dao>
+
+  beforeEach(async () => {
+    sandbox = sinon.createSandbox()
+
+    rawDao = {
+      network: NetworksEnum.ethereumMainnet,
+      transactionHash: '0x0',
+      blockNumber: 0,
+      blockTimestamp: 1219577223,
+      address: '0x17366cae2b9c6c3055e9e3c78936a69006be5409',
+      implementationAddress: '0x17366cae2b9c6c3055e9e3c78936a69006be5409',
+      creatorAddress: '0x17366cae2b9c6c3055e9e3c78936a69006be5409',
+      ens: 'dao.eth',
+      subdomain: 'dao',
+      members: 10,
+      metadataIpfs: 'metadataIpfs',
+      name: 'fake-name',
+      description: 'fake-description',
+      avatar: 'fake-avatar',
+      links: [
+        {
+          name: 'fake-name',
+          url: 'fake-url',
+        },
+      ],
+      metrics: {
+        members: 15,
+        proposalsCreated: 5,
+        proposalsExecuted: 3,
+        uniqueVoters: 100,
+        votes: 500,
+      },
+      tvlUSD: 10000,
+      plugins: [
+        {
+          transactionHash: '0x0',
+          blockNumber: 0,
+          address: '0x0',
+          implementationAddress: '0x0',
+          tokenAddress: '0x01',
+          pluginSetupRepoAddress: '0x02',
+          release: '0',
+          build: '0',
+          subdomain: 'test',
+        },
+      ],
+      hideDao: false,
+    }
+
+    rawMember = {
+      address: '0x17366cae2b9c6c3055e9e3c78936a69006be5409',
+      ens: 'test.eth',
+      history: [
+        {
+          network: NetworksEnum.ethereumMainnet,
+          daoAddress: '0x17366cae2b9c6c3055e9e3c78936a69006be5409',
+          tokenAddress: '0x17366cae2b9c6c3055e9e3c78936a69006be5409',
+          pluginAddress: '0x12366cae2b9c6c3055e9e3c78936a69006be5409',
+          fromBlockNumber: 1,
+          toBlockNumber: 2,
+          fromTxHash: '0xBaDCAFebab823C9A60A84009702Fa4b25d6F1969',
+          toTxHash: '0xBaDCAFebab823C9A60A84009702Fa4b25d6F1969',
+          delegateFromAddress: '0x17366cae2b9c6c3055e9e3c78936a69006be5409',
+          delegateToAddress: '0x17366cae2b9c6c3055e9e3c78936a69006be5409',
+          votingPower: '100',
+          pluginSubdomain: 'token-voting',
+          metrics: {
+            tokenBalance: '100',
+            delegateCount: 0,
+            voteCount: 0,
+            proposalCount: 0,
+          },
+          fromBlockTimestamp: 0,
+        },
+      ],
+    }
+
+    await Models.Member.create(rawMember as any)
+    await Models.Dao.create(rawDao as any)
+  })
+
+  afterEach(() => {
+    sandbox.restore()
+  })
+
+  describe('pairFromPaginationParams', () => {
+    it('should pairFromPaginationParams with ens', async () => {
+      const paginationParams = { search: rawMember.ens }
+      const result = await PairDataModule.pairFromPaginationParams(paginationParams)
+      expect(result.search).to.be.equal(rawMember.address)
+    })
+
+    it('should pairFromPaginationParams with ens - not a member', async () => {
+      const paginationParams = { search: 'test1.eth' }
+      const result = await PairDataModule.pairFromPaginationParams(paginationParams)
+      expect(result.search).to.be.equal(paginationParams.search)
+    })
+
+    it('should pairFromPaginationParams with no ens', async () => {
+      const paginationParams = { search: 'test1' }
+      const result = await PairDataModule.pairFromPaginationParams(paginationParams)
+      expect(result.search).to.be.equal(paginationParams.search)
+    })
+  })
+
+  describe('pairFromExtraParams', () => {
+    it('should pairFromExtraParams with daoId', async () => {
+      const extraParams = {}
+      const pairParams = {
+        daoId: Models.Dao.getEntityId({ network: rawDao.network, address: rawDao.address } as any),
+        ens: rawMember.ens,
+      }
+
+      const result: any = await PairDataModule.pairFromExtraParams(extraParams, pairParams)
+
+      expect(result.network).to.be.equal(rawDao.network)
+      expect(result.daoAddress).to.be.equal(rawDao.address)
+      expect(result.memberAddress).to.be.equal(rawMember.address)
+    })
+
+    it('should pairFromExtraParams - no dao, ens', async () => {
+      const extraParams = {}
+      const pairParams = { daoId: 'fake-id', ens: 'fake-ens.eth' }
+
+      const result: any = await PairDataModule.pairFromExtraParams(extraParams, pairParams)
+
+      expect(result.network).to.be.undefined
+      expect(result.daoAddress).to.be.undefined
+      expect(result.memberAddress).to.be.undefined
+    })
+  })
+})
