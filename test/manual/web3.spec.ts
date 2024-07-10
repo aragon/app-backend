87e82be07068c5dc17d7eca25c4055891dc77ffc
Sishir Pokhrel <sisnet2010@gmail.com>
2024-07-10 17:04:24 +0545

    ens resolving with alchemy

--- test/manual/web3.spec.ts
+++ test/manual/web3.spec.ts
@@ -17,21 +17,26 @@ describe('Manual: Web3', () => {
     sandbox && sandbox.restore()
   })
 
-  it('should get address from ens', async () => {
-    config.BLOCKCHAIN_NODES.ETHEREUM_MAINNET = ''
+  it.only('should get address from ens', async () => {
+    config.BLOCKCHAIN_NODES.ETHEREUM_MAINNET = 'wss://eth-mainnet.g.alchemy.com/v2/lWAIOIIa7ianbxxJdqloK66gbTJN2wzQ'
     await ProviderModule.connectToAllNetworks()
-    const aavegotchiAddress = '0xF1cf9aFc900Ce3426A235212e164587A6274736A'
-    const name = 'aavegotchi.dao.eth'
+    const aavegotchiAddress = '0x42E6DD8D517abB3E4f6611Ca53a8D1243C183fB0'
+    const name = 'leuts.eth'
     const address = await Web3Helper.getAddressFromEns(name, NetworksEnum.ethereumMainnet)
     expect(address).to.eq(aavegotchiAddress)
   })
 
-  it.skip('should get ens from address', async () => {
-    config.BLOCKCHAIN_NODES.ETHEREUM_MAINNET = ''
+  it.only('should get ens from address', async () => {
+    config.BLOCKCHAIN_NODES.ETHEREUM_MAINNET = 'wss://eth-mainnet.g.alchemy.com/v2/lWAIOIIa7ianbxxJdqloK66gbTJN2wzQ'
     await ProviderModule.connectToAllNetworks()
-    const aavegotchiAddress = '0xF1cf9aFc900Ce3426A235212e164587A6274736A'
-    const name = 'aavegotchi.dao.eth'
-    const ensName = await Web3Helper.getEnsFromAddress(aavegotchiAddress, NetworksEnum.ethereumMainnet)
-    expect(ensName).to.eq(name)
+
+    //leuts.eth 0x42E6DD8D517abB3E4f6611Ca53a8D1243C183fB0
+    // sio.eth 0xd5fb864ACfD6BB2f72939f122e89fF7F475924f5
+
+    const aavegotchiAddress = '0x42E6DD8D517abB3E4f6611Ca53a8D1243C183fB0'
+    const name = 'leuts.eth'
+    const ensName = await Web3Helper.getEnsWithAlchemy(aavegotchiAddress)
+    console.log(ensName)
+    // expect(ensName).to.eq(name)
   })
 })
