9374843d4d8c36bf54766fbb335550f4cba321ca
cristianizzo <cristianizzo@me.com>
2024-07-11 04:49:07 +0200

    setting token

--- src/models/schema/setting.ts
+++ src/models/schema/setting.ts
@@ -15,6 +15,9 @@ import ModelUtils from '@models/utils/models'
 const customName = 'Setting'
 
 class Token {
+  @prop({ type: () => String, enum: NetworksEnum })
+  public network!: NetworksEnum
+
   @prop({ type: () => String, enum: ITokenType, required: true })
   public type!: ITokenType
 
@@ -32,6 +35,9 @@ class Token {
 
   @prop({ type: () => Number, default: 18 })
   public decimals!: number
+
+  @prop({ type: () => String, default: '0' })
+  public totalSupply!: string
 }
 
 class Settings {
