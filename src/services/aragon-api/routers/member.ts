a291f75e01a6e404fb6f7ecae43ca5da6926f3fd
cristianizzo <cristianizzo@me.com>
2024-08-14 11:55:56 +0200

    onlyActive fix boolean

--- src/services/aragon-api/routers/member.ts
+++ src/services/aragon-api/routers/member.ts
@@ -17,7 +17,7 @@ const MemberRouter = {
   getMembersWithPagination: async function (ctx: RouterContext) {
     const paginationParams = ModelUtils.parsePaginationParams(ctx, { defaultSort: 'fromBlockNumber' })
     const extraParams: IMemberExtraParams = {
-      onlyActive: ctx.query.onlyActive !== undefined && Boolean(ctx.query.onlyActive),
+      onlyActive: ctx.query.onlyActive === 'true' && Boolean(ctx.query.onlyActive),
       network: ctx.query.network as NetworksEnum,
       daoAddress: ctx.query.daoAddress as HexAddress,
       pluginAddress: ctx.query.pluginAddress as HexAddress,
@@ -50,7 +50,7 @@ const MemberRouter = {
       address: ctx.params.address,
     }
     const extraParams: IMemberExtraParams = {
-      onlyActive: ctx.query.onlyActive !== undefined && Boolean(ctx.query.onlyActive),
+      onlyActive: ctx.query.onlyActive === 'true' && Boolean(ctx.query.onlyActive),
       network: ctx.query.network as NetworksEnum,
       daoAddress: ctx.query.daoAddress as HexAddress,
       pluginAddress: ctx.query.pluginAddress as HexAddress,
