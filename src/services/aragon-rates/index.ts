09ae2eefb3dc91df3f67c75cd13b24f616491faa
cristianizzo <cristianizzo@me.com>
2024-08-23 12:53:57 +0200

    metrics

--- src/services/aragon-rates/index.ts
+++ src/services/aragon-rates/index.ts
@@ -3,7 +3,6 @@ import { EnumConnection, type IService } from '@types'
 import { TaskSchedulerState } from '@state/taskSchedulerState'
 import config from '@config'
 import { FetchRates } from '@services/aragon-rates/fetchRates'
-import { DaoAssets } from '@rates/daoAsset'
 import { DaoTransactions } from '@rates/daoTransaction'
 
 const llo = logger.logMeta.bind(null, { service: 'service:RatesService' })
@@ -14,7 +13,7 @@ const RatesService: IService = {
   start: async function () {
     logger.info('RatesService service sync start', llo({}))
 
-    const tasks = [[{ fetchRates: FetchRates }], [{ daoAssets: DaoAssets }], [{ daoTransactions: DaoTransactions }]]
+    const tasks = [[{ fetchRates: FetchRates }], [{ daoTransactions: DaoTransactions }]]
 
     const taskOptions = {
       fn: () => [...tasks],
