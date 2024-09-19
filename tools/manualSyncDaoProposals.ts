a31af37c9fd438e0e8608a4b2485fd38bbbef27e
cristianizzo <cristianizzo@me.com>
2024-09-19 20:51:27 +0200

    handleAlchemyCrazyBalance replace tvl calculation

---
+++ tools/manualSyncDaoProposals.ts
@@ -0,0 +1,36 @@
+import { EnumConnection, type IService } from '@types'
+import { Models } from '@dbModels'
+import { LogTokenVoting } from '@indexer/logTokenVoting'
+import { LogMultisig } from '@indexer/logMultisig'
+import { IPluginActionType } from '@indexer/handlers/pluginSetupProcessorHandler'
+
+export const ToolsManualSyncDaoProposals: IService = {
+  NEED_CONNECTIONS: [EnumConnection.MONGODB, EnumConnection.BLOCKCHAIN],
+
+  start: async () => {
+    const plugins = await Models.Plugin.find({
+      daoAddress: '',
+      type: IPluginActionType.installed,
+    })
+
+    for (const plugin of plugins) {
+      const setting = await Models.Setting.findActive({
+        daoAddress: plugin.daoAddress,
+        pluginAddress: plugin.address,
+        network: plugin.network,
+      })
+
+      if (setting) {
+        if (setting.tokenAddress) {
+          await LogTokenVoting.start(plugin)
+        } else {
+          await LogMultisig.start(plugin)
+        }
+      }
+    }
+  },
+
+  stop: async () => {},
+}
+
+export default ToolsManualSyncDaoProposals
