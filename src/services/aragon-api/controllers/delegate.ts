0feb86cb8a59aecdfe1c6536ce1d370785f4bbd8
CryptoCrash <cristianizzo@me.com>
2024-09-20 04:35:47 +0200

    Feat/endpoint optimisation (#158)
    * optimise dao findWithPagination
    
    * optimise dao findWithPagination
    
    * optimise dao findWithPagination

--- src/services/aragon-api/controllers/delegate.ts
+++ src/services/aragon-api/controllers/delegate.ts
@@ -7,6 +7,7 @@ import {
   type IPairParams,
 } from '@types'
 import PairDataModule from '@modules/pairData'
+import type DaoMemberMapping from '@models/schema/daoMemberMapping'
 
 const DelegateController = {
   getDelegateWithPagination: async (
@@ -15,7 +16,19 @@ const DelegateController = {
     pairParams: IPairParams = {},
   ): Promise<IPaginatedResult<IDelegatesResponse>> => {
     extraParams = await PairDataModule.pairFromExtraParams(extraParams, pairParams)
-    const result = await Models.MemberTransaction.findWithPagination({ extraParams, paginationParams })
+    const mapping = await PairDataModule.pairFromDaoMemberMapping({
+      daoAddress: extraParams.daoAddress,
+      pluginAddress: extraParams.pluginAddress,
+      network: extraParams.network,
+    })
+
+    const memberAddresses = mapping.map((w: DaoMemberMapping) => w.memberAddress)
+
+    const result = await Models.MemberTransaction.findWithPagination({
+      extraParams,
+      paginationParams,
+      extraQueryData: { memberAddresses },
+    })
 
     return result
   },
