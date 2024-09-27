6606c1de55e331f9610d7181743234cc063fb249
cristianizzo <cristianizzo@me.com>
2024-09-27 14:36:32 +0200

    better handle future block issue

--- src/helpers/retryRequest.ts
+++ src/helpers/retryRequest.ts
@@ -29,7 +29,7 @@ export async function retryRequest<T>(requestFunction: () => Promise<T>, options
         )
         await Utils.wait(retryDelay(retryCount))
         retryCount++
-      } else if (error?.reason.includes('future lookup')) {
+      } else if (error?.reason?.includes('future lookup')) {
         logger.warn(
           'ForceRetry, retrying...',
           llo({ retryCount, wait: retryDelay(retryCount), fn: requestFunction.toString(), error }),
