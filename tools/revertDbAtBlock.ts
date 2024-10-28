973dd4838e35c2b4d0d8e1a868c2f20897fe9ccf
cristianizzo <cristianizzo@me.com>
2024-10-28 21:06:15 +0100

    tool to revertDbAtBlock

---
+++ tools/revertDbAtBlock.ts
@@ -0,0 +1,68 @@
+import { EnumConnection, type IService, NetworksEnum } from '@types'
+import { Models } from '@dbModels'
+import { NetworkHelper } from '@helpers/network'
+
+export const ToolsRevertDbAtBlock: IService = {
+  NEED_CONNECTIONS: [EnumConnection.MONGODB, EnumConnection.BLOCKCHAIN],
+
+  start: async () => {
+    const config = {
+      [NetworksEnum.ethereumSepolia]: 6876125,
+      // add additional network block number configs as needed
+    }
+
+    const networks = NetworkHelper.supportedNetworks()
+
+    await Promise.all(
+      networks.map(async ({ networkName }) => {
+        const atBlockNumber = config[networkName]
+        if (!atBlockNumber) {
+          console.log(`No blockNumber configured for ${networkName}. Skipping.`) // eslint-disable-line no-console
+          return
+        }
+
+        await Promise.all(
+          Object.keys(Models).map(async modelName => {
+            const model = Models[modelName]
+            if (model.deleteMany) {
+              const hasBlockNumberField = await model.exists({ blockNumber: { $exists: true } })
+              if (!hasBlockNumberField) {
+                console.log(`Model ${modelName} does not have a blockNumber field. Skipping.`) // eslint-disable-line no-console
+                return
+              }
+
+              try {
+                const result = await model.deleteMany({
+                  network: networkName,
+                  blockNumber: { $gt: atBlockNumber },
+                })
+                console.log(
+                  `Deleted ${result.deletedCount} documents in model ${modelName} for ${networkName} with blockNumber > ${atBlockNumber}`,
+                ) // eslint-disable-line no-console
+              } catch (err) {
+                console.error(`Error deleting documents in model ${modelName} for ${networkName}:`, err) // eslint-disable-line no-console
+              }
+            }
+          }),
+        )
+
+        // Update Models.ConfigIndexer with lastSync
+        try {
+          const updateResult = await Models.ConfigIndexer.updateMany(
+            { network: networkName },
+            { $set: { lastSync: atBlockNumber } },
+          )
+          console.log(
+            `Updated lastSync to ${atBlockNumber} in ConfigIndexer for ${networkName}, matched ${updateResult.matchedCount}, modified ${updateResult.modifiedCount}`,
+          ) // eslint-disable-line no-console
+        } catch (err) {
+          console.error(`Error updating lastSync in ConfigIndexer for ${networkName}:`, err) // eslint-disable-line no-console
+        }
+      }),
+    )
+  },
+
+  stop: async () => {},
+}
+
+export default ToolsRevertDbAtBlock
