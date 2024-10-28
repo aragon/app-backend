b94f88303b603a4def33d7f3e2b331e18c239e06
cristianizzo <cristianizzo@me.com>
2024-10-28 15:12:24 +0100

    expose lastStageTransition

--- src/models/schema/daoMemberMapping.ts
+++ src/models/schema/daoMemberMapping.ts
@@ -187,6 +187,7 @@ export default class DaoMemberMapping extends Model {
           totalStages: 1,
           subPlugins: 1,
           stageIndex: 1,
+          lastStageTransition: 1,
           parentPlugin: 1,
         },
         {
--- src/types/aggregation.ts
+++ src/types/aggregation.ts
@@ -150,6 +150,7 @@ export interface IAggPluginProjectFields {
   totalStages: 1
   subPlugins: 1
   stageIndex: 1
+  lastStageTransition: 1
   parentPlugin: 1
   permissions?: 1
   uninstalled?: 1
