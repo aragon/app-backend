a612289def942dcff05f92f7cf3911a93ff7a02b
cristianizzo <cristianizzo@me.com>
2024-11-19 12:09:30 +0700

    format

--- src/services/aragon-dao/index.ts
+++ src/services/aragon-dao/index.ts
@@ -42,6 +42,12 @@ const DaoSyncService: IService = {
       await ProposalMetrics.proposalTokenVotingMetrics({ proposalIndex, pluginAddress, network })
     })
 
+    await RabbitMQHelper.process(EnumQueueName.proposalTokenVotingMetrics, 10, async job => {
+      const { proposalIndex, pluginAddress, network } = job.params as IQueueProposalMetrics
+
+      await ProposalMetrics.proposalTokenVotingMetrics({ proposalIndex, pluginAddress, network })
+    })
+
     logger.info('DaoSyncService service started', llo({}))
   },
 
