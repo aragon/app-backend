d8abe7ea800db4e89340c22a4b37c35cc734a4a1
cristianizzo <cristianizzo@me.com>
2024-12-18 16:16:44 +0100

    unit test + increase coverage

---
+++ test/unit/services/aragon-api/middlewares/error.spec.ts
@@ -0,0 +1,84 @@
+import * as sinon from 'sinon'
+import { SinonSandbox } from 'sinon'
+import { expect } from 'chai'
+import ErrorMiddleware from '@services/aragon-api/middlewares/error'
+import { ErrorKeyEnum } from '@types'
+
+describe('middlewares: error', () => {
+  let sandbox: SinonSandbox
+
+  beforeEach(async () => {
+    sandbox = sinon.createSandbox()
+  })
+
+  afterEach(() => {
+    sandbox?.restore()
+  })
+
+  it('run', async () => {
+    const next = sinon.stub() as any
+    const ctx = {} as any
+
+    await ErrorMiddleware()(ctx, next)
+
+    expect(next.calledWith()).to.be.true
+  })
+
+  it('catch', async () => {
+    const next = sinon.stub().rejects(new Error('coucou')) as any
+    const ctx = { requestInfo: {} } as any
+
+    await ErrorMiddleware()(ctx, next)
+
+    expect(next.calledWith()).to.be.true
+    expect(ctx.status).to.eq(500)
+    expect(ctx.body).to.deep.eq({
+      code: ErrorKeyEnum.unknownError,
+      description: 'Internal server error',
+    })
+  })
+
+  it('catch with custom not found', async () => {
+    const err = new Error(ErrorKeyEnum.badParams) as any
+    err.exposeCustom_ = true
+    err.status = 100
+    err.description = 'desc'
+    err.exposeMeta = 'meta'
+    const next = sinon.stub().rejects(err) as any
+    const ctx = { requestInfo: {} } as any
+
+    await ErrorMiddleware()(ctx, next)
+
+    expect(next.calledWith()).to.be.true
+
+    expect(ctx.status).to.eq(400)
+    expect(ctx.body).to.deep.eq({
+      code: ErrorKeyEnum.badParams,
+      description: 'desc',
+      meta: 'meta',
+      status: 400,
+    })
+  })
+
+  it('catch with custom not found', async () => {
+    const err = new Error('coucou') as any
+    err.exposeCustom_ = true
+    err.status = 100
+    err.description = 'desc'
+    err.exposeMeta = 'meta'
+    const next = sinon.stub().rejects(err) as any
+    const ctx = { requestInfo: {} } as any
+
+    await ErrorMiddleware()(ctx, next)
+
+    expect(next.calledWith()).to.be.true
+
+    expect(ctx.status).to.eq(500)
+    expect(ctx.body).to.deep.eq({
+      code: ErrorKeyEnum.unknownError,
+      description: 'desc',
+      meta: 'meta',
+      status: 500,
+    })
+  })
+})
