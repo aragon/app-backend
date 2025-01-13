a8cb17d88394e0f8c20a96ddf278b53bf739e230
sishir <sisnet2010@gmail.com>
2025-01-13 16:28:32 +0545

    test fixed

--- test/unit/helpers/web3.spec.ts
+++ test/unit/helpers/web3.spec.ts
@@ -1425,7 +1425,6 @@ describe('Helpers:Web3', () => {
       } as any)
 
       await Web3Helper.getBlockReceipts(12321, fakeNetwork)
-      await Web3Helper.getBlockReceipts(12321, fakeNetwork)
       expect(providerStub.send.calledOnce).to.be.true
       expect(providerStub.send.calledWith('eth_getBlockReceipts', [`0x${(12321).toString(16)}`])).to.be.true
     })
