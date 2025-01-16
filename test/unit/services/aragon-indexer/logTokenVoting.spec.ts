c35cc74c427b19caa3d6390020086228ab077863
cristianizzo <cristianizzo@me.com>
2025-01-16 01:55:47 +0100

    unit test

--- test/unit/services/aragon-indexer/logTokenVoting.spec.ts
+++ test/unit/services/aragon-indexer/logTokenVoting.spec.ts
@@ -7,6 +7,7 @@ import BlockchainLogCrawler from '@modules/blockchainLogCrawler'
 import { NetworksEnum } from '@types'
 import { UnitTestUtils } from '@test/lib/utils'
 import ProviderModule from '@modules/provider'
+import {ProxyToken} from "@modules/proxyToken";
 
 describe('Indexer: LogTokenVoting', () => {
   let sandbox: SinonSandbox
@@ -31,200 +32,14 @@ describe('Indexer: LogTokenVoting', () => {
 
       const crawlStub = sandbox.stub(BlockchainLogCrawler.prototype, 'crawl').resolves()
       const verboseStub = sandbox.stub(logger, 'verbose')
+      const stubToken = sandbox.stub(ProxyToken, 'saveAndGetToken').resolves({blockNumber: 10} as any)
 
       await LogTokenVoting.start(plugin)
 
-      expect(crawlStub.calledOnce).to.be.true
+      expect(stubToken.calledOnceWith(plugin.tokenAddress, plugin.network)).to.be.true
+      expect(crawlStub.calledTwice).to.be.true
       expect(verboseStub.calledWith('Start LogTokenVoting' as any)).to.be.true
       expect(verboseStub.calledWith('End LogTokenVoting' as any)).to.be.true
     })
   })
-
-  // describe('should process log', () => {
-  //   it('should process the vote cast log', async () => {
-  //     const txLog = {
-  //       data: '0x1234567890123456789012345678901234567890',
-  //       topics: ['0x1234567890123456789012345678901234567890'],
-  //     } as any
-  //
-  //     const network = NetworksEnum.polygonMainnet
-  //     const verboseStub = sandbox.stub(logger, 'verbose')
-  //     const parseLogStub = sandbox.stub(Web3Helper, 'parseLog').returns({ name: 'VoteCast' } as any)
-  //     const voteCastStub = sandbox.stub(ProposalHandler, 'voteCast').resolves()
-  //     const parseInfoLogStub = sandbox.stub(Web3Helper, 'parseInfoLog').returns({
-  //       network: network,
-  //       blockNumber: 123213,
-  //       transactionHash: '0x1234567890123456789012345678901234567890',
-  //     } as any)
-  //
-  //     await LogTokenVoting.processLog(txLog, network)
-  //
-  //     expect(verboseStub.calledWith('VoteCast' as any)).to.be.true
-  //     expect(parseLogStub.calledOnce).to.be.true
-  //     expect(parseInfoLogStub.calledOnce).to.be.true
-  //     expect(voteCastStub.calledOnce).to.be.true
-  //   })
-  //
-  //   it('should process the proposal created log', async () => {
-  //     const txLog = {
-  //       data: '0x1234567890123456789012345678901234567890',
-  //       topics: ['0x1234567890123456789012345678901234567890'],
-  //     } as any
-  //
-  //     const network = NetworksEnum.polygonMainnet
-  //     const verboseStub = sandbox.stub(logger, 'verbose')
-  //     const parseLogStub = sandbox.stub(Web3Helper, 'parseLog').returns({ name: 'ProposalCreated' } as any)
-  //     const proposalCreatedStub = sandbox.stub(ProposalHandler, 'proposalCreated').resolves()
-  //     const parseInfoLogStub = sandbox.stub(Web3Helper, 'parseInfoLog').returns({
-  //       network: network,
-  //       blockNumber: 123213,
-  //       transactionHash: '0x1234567890123456789012345678901234567890',
-  //     } as any)
-  //
-  //     await LogTokenVoting.processLog(txLog, network)
-  //
-  //     expect(verboseStub.calledWith('ProposalCreated' as any)).to.be.true
-  //     expect(parseLogStub.calledOnce).to.be.true
-  //     expect(parseInfoLogStub.calledOnce).to.be.true
-  //     expect(proposalCreatedStub.calledOnce).to.be.true
-  //   })
-  //
-  //   it('should process the proposal executed log', async () => {
-  //     const txLog = {
-  //       data: '0x1234567890123456789012345678901234567890',
-  //       topics: ['0x1234567890123456789012345678901234567890'],
-  //     } as any
-  //
-  //     const network = NetworksEnum.polygonMainnet
-  //     const verboseStub = sandbox.stub(logger, 'verbose')
-  //     const parseLogStub = sandbox.stub(Web3Helper, 'parseLog').returns({ name: 'ProposalExecuted' } as any)
-  //     const proposalExecutedStub = sandbox.stub(ProposalHandler, 'proposalExecuted').resolves()
-  //     const parseInfoLogStub = sandbox.stub(Web3Helper, 'parseInfoLog').returns({
-  //       network: network,
-  //       blockNumber: 123213,
-  //       transactionHash: '0x1234567890123456789012345678901234567890',
-  //     } as any)
-  //
-  //     await LogTokenVoting.processLog(txLog, network)
-  //
-  //     expect(verboseStub.calledWith('ProposalExecuted' as any)).to.be.true
-  //     expect(parseLogStub.calledOnce).to.be.true
-  //     expect(parseInfoLogStub.calledOnce).to.be.true
-  //     expect(proposalExecutedStub.calledOnce).to.be.true
-  //   })
-  //
-  //   it('should process the voting settings updated log', async () => {
-  //     const txLog = {
-  //       data: '0x1234567890123456789012345678901234567890',
-  //       topics: ['0x1234567890123456789012345678901234567890'],
-  //     } as any
-  //
-  //     const network = NetworksEnum.polygonMainnet
-  //     const verboseStub = sandbox.stub(logger, 'verbose')
-  //     const parseLogStub = sandbox.stub(Web3Helper, 'parseLog').returns({ name: 'VotingSettingsUpdated' } as any)
-  //     const votingSettingsUpdatedStub = sandbox.stub(PluginSettingHandler, 'votingSettingsUpdated').resolves()
-  //     const parseInfoLogStub = sandbox.stub(Web3Helper, 'parseInfoLog').returns({
-  //       network: network,
-  //       blockNumber: 123213,
-  //       transactionHash: '0x1234567890123456789012345678901234567890',
-  //     } as any)
-  //
-  //     await LogTokenVoting.processLog(txLog, network)
-  //
-  //     expect(verboseStub.calledWith('VotingSettingsUpdated' as any)).to.be.true
-  //     expect(parseLogStub.calledOnce).to.be.true
-  //     expect(parseInfoLogStub.calledOnce).to.be.true
-  //     expect(votingSettingsUpdatedStub.calledOnce).to.be.true
-  //   })
-  //
-  //   it('should process the transfer log', async () => {
-  //     const txLog = {
-  //       data: '0x1234567890123456789012345678901234567890',
-  //       topics: ['0x1234567890123456789012345678901234567890'],
-  //     } as any
-  //
-  //     const network = NetworksEnum.polygonMainnet
-  //     const verboseStub = sandbox.stub(logger, 'verbose')
-  //
-  //     const fakeLogInfo = {
-  //       network: network,
-  //       blockNumber: 123213,
-  //       transactionIndex: 1,
-  //       logIndex: 1,
-  //       transactionHash: '0x1234567890123456789012345678901234567890',
-  //       address: '0x1234567890123456789012345678901234567890',
-  //       eventName: 'Transfer',
-  //     }
-  //
-  //     const parseLogStub = sandbox.stub(Web3Helper, 'parseLog').returns({ name: 'Transfer' } as any)
-  //
-  //     const parseInfoLogStub = sandbox.stub(Web3Helper, 'parseInfoLog').returns(fakeLogInfo)
-  //
-  //     const GovernanceErc20HandlerStub = sandbox.stub(GovernanceErc20Handler, 'transfer').resolves()
-  //
-  //     await LogTokenVoting.processLog(txLog, network)
-  //
-  //     expect(parseLogStub.calledOnce).to.be.true
-  //     expect(parseInfoLogStub.calledOnce).to.be.true
-  //     expect(GovernanceErc20HandlerStub.calledOnce).to.be.true
-  //     expect(parseInfoLogStub.calledWith(txLog, 'Transfer', network)).to.be.true
-  //     expect(verboseStub.calledWith('Transfer' as any)).to.be.true
-  //   })
-  //
-  //   it('should process the delegate votes changed log', async () => {
-  //     const txLog = {
-  //       data: '0x1234567890123456789012345678901234567890',
-  //       topics: ['0x1234567890123456789012345678901234567890'],
-  //     } as any
-  //
-  //     const network = NetworksEnum.polygonMainnet
-  //     const verboseStub = sandbox.stub(logger, 'verbose')
-  //
-  //     const fakeLogInfo = {
-  //       network: network,
-  //       blockNumber: 123213,
-  //       transactionIndex: 1,
-  //       logIndex: 1,
-  //       transactionHash: '0x1234567890123456789012345678901234567890',
-  //       address: '0x1234567890123456789012345678901234567890',
-  //       eventName: 'DelegateVotesChanged',
-  //     }
-  //
-  //     const parseLogStub = sandbox.stub(Web3Helper, 'parseLog').returns({ name: 'DelegateVotesChanged' } as any)
-  //
-  //     const parseInfoLogStub = sandbox.stub(Web3Helper, 'parseInfoLog').returns(fakeLogInfo)
-  //
-  //     const GovernanceErc20HandlerStub = sandbox.stub(GovernanceErc20Handler, 'delegateVotesChanged').resolves()
-  //
-  //     await LogTokenVoting.processLog(txLog, network)
-  //
-  //     expect(parseLogStub.calledOnce).to.be.true
-  //     expect(parseInfoLogStub.calledOnce).to.be.true
-  //     expect(GovernanceErc20HandlerStub.calledOnce).to.be.true
-  //     expect(parseInfoLogStub.calledWith(txLog, 'DelegateVotesChanged', network)).to.be.true
-  //     expect(verboseStub.calledWith('DelegateVotesChanged' as any)).to.be.true
-  //   })
-  //
-  //   it('should process the unhandled event log', async () => {
-  //     const txLog = {
-  //       data: '0x1234567890123456789012345678901234567890',
-  //       topics: ['0x1234567890123456789012345678901234567890'],
-  //     } as any
-  //
-  //     const network = NetworksEnum.polygonMainnet
-  //     const errorStub = sandbox.stub(logger, 'error')
-  //     const parseLogStub = sandbox.stub(Web3Helper, 'parseLog').returns({ name: 'Unknown' } as any)
-  //     const parseInfoLogStub = sandbox.stub(Web3Helper, 'parseInfoLog').returns({
-  //       network: network,
-  //       blockNumber: 123213,
-  //       transactionHash: '0x1234567890123456789012345678901234567890',
-  //     } as any)
-  //
-  //     await LogTokenVoting.processLog(txLog, network)
-  //
-  //     expect(parseLogStub.calledOnce).to.be.true
-  //     expect(parseInfoLogStub.calledOnce).to.be.true
-  //     expect(errorStub.calledWith('Unhandled event' as any)).to.be.true
-  //   })
-  // })
 })
