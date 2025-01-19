e3e2fab6fadd12f78c314ffd5fff8cfc39447633
cristianizzo <cristianizzo@me.com>
2025-01-20 00:49:43 +0100

    unit test

--- test/unit/services/aragon-plugins/logTokenVoting.spec.ts
+++ test/unit/services/aragon-plugins/logTokenVoting.spec.ts
@@ -34,12 +34,15 @@ describe('Plugins: LogTokenVoting', () => {
     it('should start the LogDao', async () => {
       const crawlStub = sandbox.stub(BlockchainLogCrawler.prototype, 'crawl').resolves()
       const verboseStub = sandbox.stub(logger, 'verbose')
-      sandbox.stub(ProxyToken, 'saveAndGetToken').resolves({ blockNumber: 1 } as any)
-      await LogTokenVoting.start({
-        address: '0x123',
-        tokenAddress: '0x123',
-        network: NetworksEnum.ethereumSepolia,
-      } as any)
+      const token = { address: '0x123', network: NetworksEnum.ethereumSepolia } as any
+      await LogTokenVoting.start(
+        {
+          address: '0x123',
+          tokenAddress: token.address,
+          network: token.network,
+        } as any,
+        token as any,
+      )
       expect(crawlStub.calledTwice).to.be.true
       expect(verboseStub.calledWith('Start LogTokenVoting' as any)).to.be.true
       expect(verboseStub.calledTwice).to.be.true
