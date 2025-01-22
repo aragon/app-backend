ec394e80f30cf87ffc4513887023db51a3929d11
cristianizzo <cristianizzo@me.com>
2025-01-22 16:52:11 +0100

    unit test

--- tools/toolLauncher.ts
+++ tools/toolLauncher.ts
@@ -15,11 +15,13 @@ import ToolsMigratePluginSlug from '@tools/migratePluginSlug'
 import ToolsMemberMetrics from '@tools/memberMetrics'
 import ToolsEnsFetch from '@tools/ensFetch'
 import ToolsFixSettingIssue from '@tools/fixBrokenPluginSetting'
+import ToolsCleanDb from '@tools/cleanDb'
 
 const { TOOL_RUN } = process.env
 const llo = logger.logMeta.bind(null, { TOOL_RUN })
 
 const runners = {
+  ToolsCleanDb,
   ToolsEnsFetch,
   ToolsMemberMetrics,
   ToolsMigratePluginSlug,
