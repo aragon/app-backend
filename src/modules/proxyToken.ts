ce4477a3ca82fa6b5b8b7a2716b676054c340fa6
sishir <sisnet2010@gmail.com>
2025-01-28 19:23:19 +0545

    format fixed

--- src/modules/proxyToken.ts
+++ src/modules/proxyToken.ts
@@ -74,6 +74,22 @@ export const ProxyToken = {
 
       if (token.type === ITokenType.GovernanceERC20 || Web3Helper.isWhitelistedToken(token.address, token.network)) {
         const metrics = await CovalentHelper.getTokenSupplyAndHolders(tokenAddress, network)
+
+        if (
+          token.type === ITokenType.GovernanceERC20 &&
+          metrics.totalHolders === 0 &&
+          metrics.totalSupply === '0' &&
+          tokenAddress !== utils.zeroAddress
+        ) {
+          metrics.totalSupply = await Web3Helper.getTokenTotalSupply(tokenAddress, network)
+          if (metrics.totalSupply !== '0') {
+            await RabbitMQHelper.sendMessage(EnumQueueName.tokenInfo, {
+              id: `token-metrics${tokenAddress}`,
+              params: { address: tokenAddress, network },
+            })
+          }
+        }
+
         updates.holders = metrics.totalHolders
         updates.totalSupply = metrics.totalSupply
         updates.lastUpdatedAt = dayjs.utc().toDate()
