c65bd00b04fa9b16bcb3ee3fd8f07bfc9e31e90e
sishir <sisnet2010@gmail.com>
2025-02-05 20:48:58 +0545

    block handler

--- src/services/aragon-transactions/blockHandler.ts
+++ src/services/aragon-transactions/blockHandler.ts
@@ -49,7 +49,7 @@ export const BlockHandler = {
           'New confirmed incoming transaction',
           llo({
             network,
-            daoAddress: dao,
+            daoAddress: dao.address,
             transactionHash,
           }),
         )
