3c72c98146734db74423e33e6af38e8b04677efc
cristianizzo <cristianizzo@me.com>
2025-02-15 18:23:15 +0100

    bug member api + unit test

--- src/models/schema/dao.ts
+++ src/models/schema/dao.ts
@@ -194,13 +194,13 @@ export default class Dao extends Model {
     filter.isHidden = { $ne: true }
     filter.isActive = { $eq: true }
 
-    if (extraQueryData.daoAddresses) {
-      if (extraQueryData.daoAddresses.length > 0) {
-        filter.address = { $in: extraQueryData.daoAddresses }
-      } else {
-        // no dao for member
-        return ModelUtils.paginateEmptyResponse(request.limit)
-      }
+    if (extraQueryData.daoAddresses?.length! > 0) {
+      filter.address = { $in: extraQueryData.daoAddresses }
+    }
+
+    if (extraParams.memberAddress && extraQueryData.daoAddresses?.length === 0) {
+      // no dao for member
+      return ModelUtils.paginateEmptyResponse(request.limit)
     }
 
     const aggQuery = [
