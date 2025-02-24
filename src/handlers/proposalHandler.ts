6b20b766370529571adb0d51ec262fbfb8acc98d
sishir <sisnet2010@gmail.com>
2025-02-24 15:43:42 +0545

    advance proposal unit:test bogus stuffs

--- src/handlers/proposalHandler.ts
+++ src/handlers/proposalHandler.ts
@@ -504,11 +504,15 @@ export const ProposalHandler = {
       const subPlugins = plugin.subPlugins.find((subPlugin: { stageIndex: any }) => subPlugin.stageIndex === newStage)
 
       const timestamp = (await Web3Helper.getBlockTimestamp(info.blockNumber, info.network)) || undefined
+      const previousStageSubProposals = proposal.subProposals.filter(
+        (subProposal: any) => subProposal.stageIndex === newStage - 1,
+      )
+
       /**
        * We need to mark as executed all the sub proposals of the previous stage
        */
       await Promise.all(
-        proposal.subProposals.map(async (subProposal: any) => {
+        previousStageSubProposals.map(async (subProposal: any) => {
           const subProposalDb = await Models.Proposal.findByProposalIndex(
             subProposal.proposalIndex,
             subProposal.pluginAddress,
