d101a2edef5a625ca126166fedba0bd254216e6b
sishir <sisnet2010@gmail.com>
2025-02-26 18:39:01 +0545

    tool ready to fix the transaction

--- tools/manualSyncDaoTransactions.ts
+++ tools/manualSyncDaoTransactions.ts
@@ -1,31 +1,162 @@
-import { EnumConnection, type IService } from '@types'
+import {
+  EnumConnection,
+  type IAlchemyTransferResponse,
+  IEnumIndexerService,
+  type IService,
+  ITransactionType,
+} from '@types'
 import { Models } from '@dbModels'
 import { DaoTransactions } from '@services/aragon-dao/daoTransactions'
-import type Dao from '@models/schema/dao'
+
 import logger from '@logger'
+import { NetworkHelper } from '@helpers/network'
+import DBCrawler from '@models/utils/crawler'
+import BlockchainTransferCrawler from '@modules/blockchainTransferCrawler'
 
 const llo = logger.logMeta.bind(null, { service: 'tools:manualSyncDaoTransactions' })
 
-export const ToolsManualSyncDaoTransactions: IService = {
+interface IDaoTransactionsTools extends IService {
+  onDocument: any
+  fixOldTransactions: any
+}
+export const ToolsManualSyncDaoTransactions: IDaoTransactionsTools = {
   NEED_CONNECTIONS: [EnumConnection.MONGODB, EnumConnection.BLOCKCHAIN],
 
   start: async () => {
-    const daos = await Models.Dao.find()
+    const networks = NetworkHelper.supportedNetworks()
 
     await Promise.all(
-      daos.map(async (dao: Dao) => {
-        const configIndexer = await Models.ConfigIndexer.find({
-          service: { $regex: `(deposit|withdraw)-${daos}` },
+      networks.map(async ({ networkName }) => {
+        const dbCrawler = new DBCrawler({
+          model: Models.Transaction,
+          onDocument: ToolsManualSyncDaoTransactions.onDocument,
+          onError: (error: any, document: any) => {
+            logger.error('Error Sync all Transaction', { document, error })
+          },
+          batchSize: 500,
+          concurrency: 10,
+          useAggregate: true,
+          aggregate: (_skip: number | undefined, _limit: number | undefined) => {
+            return [
+              {
+                $match: {
+                  network: networkName,
+                },
+              },
+              {
+                $group: {
+                  _id: {
+                    daoAddress: '$daoAddress',
+                    network: '$network',
+                  },
+                },
+              },
+              {
+                $skip: _skip ?? 0,
+              },
+              {
+                $limit: _limit ?? 500,
+              },
+              {
+                $project: {
+                  _id: 0,
+                  daoAddress: '$_id.daoAddress',
+                  network: '$_id.network',
+                },
+              },
+            ]
+          },
         })
 
-        for (const config of configIndexer) {
-          await config.update({ lastSync: dao.blockNumber })
-        }
-
-        await DaoTransactions.start({ daoAddress: dao.address, network: dao.network })
-        logger.verbose('End manualSyncDaoTransactions', llo({ daoId: dao.id, daoAddress: dao.address }))
+        await dbCrawler.crawl()
       }),
     )
+  },
+  onDocument: async (dao: any) => {
+    const daoDb = await Models.Dao.findByAddress(dao.daoAddress, dao.network)
+    if (!daoDb) return
+
+    // fix block number first
+    const logDepositService = `deposit-${dao.address}-${IEnumIndexerService.depositTxs}`
+    const depoConfigIndexerDb = await Models.ConfigIndexer.findOne({
+      service: logDepositService,
+      network: dao.network,
+    })
+
+    await depoConfigIndexerDb?.update({
+      lastSync: daoDb.blockNumber,
+    })
+
+    const logWithdrawService = `withdraw-${dao.address}-${IEnumIndexerService.withdrawTxs}`
+    const withdrawConfigIndexerDb = await Models.ConfigIndexer.findOne({
+      service: logWithdrawService,
+      network: dao.network,
+    })
+
+    await withdrawConfigIndexerDb?.update({
+      lastSync: daoDb.blockNumber,
+    })
+
+    logger.verbose('Previous block number cleard', llo({ dao: dao.daoAddress }))
+
+    const category = DaoTransactions.getCategories(dao.network)
+    const depositTxCrawler = new BlockchainTransferCrawler({
+      network: dao.network,
+      filter: {
+        toAddress: dao.address,
+        fromBlock: dao.blockNumber,
+        category,
+      },
+      onTx: async (txLog: IAlchemyTransferResponse) => {
+        await ToolsManualSyncDaoTransactions.fixOldTransactions(txLog, daoDb)
+        await DaoTransactions.saveTransaction(txLog, ITransactionType.deposit, daoDb)
+      },
+
+      onError: async (error: any) => {
+        logger.error(
+          'Error deposit transfer',
+          llo({ error, type: ITransactionType.withdraw, daoId: dao.id, network: dao.network }),
+        )
+      },
+      logService: `deposit-${dao.address}-${IEnumIndexerService.depositTxs}` as any,
+      stopOnError: true,
+    })
+
+    // txs from daoAddress
+    const withdrawTxCrawler = new BlockchainTransferCrawler({
+      network: dao.network,
+      filter: {
+        fromAddress: dao.address,
+        fromBlock: dao.blockNumber,
+        category,
+      },
+      onTx: async (txLog: IAlchemyTransferResponse) => {
+        await ToolsManualSyncDaoTransactions.fixOldTransactions(txLog, daoDb)
+        return await DaoTransactions.saveTransaction(txLog, ITransactionType.withdraw, daoDb)
+      },
+      onError: async (error: any) => {
+        logger.error(
+          'Error withdraw transfer',
+          llo({ error, type: ITransactionType.withdraw, daoId: dao.id, network: dao.network }),
+        )
+      },
+      logService: `withdraw-${dao.address}-${IEnumIndexerService.withdrawTxs}` as any,
+      stopOnError: true,
+    })
+
+    await Promise.all([depositTxCrawler.crawl(), withdrawTxCrawler.crawl()])
+  },
+
+  fixOldTransactions: async (txLog: IAlchemyTransferResponse, dao: any) => {
+    const oldTx = await Models.Transaction.findOne({
+      transactionHash: txLog.hash,
+      network: dao.network,
+    })
+
+    if (oldTx && !oldTx.uniqueId) {
+      await oldTx.remove()
+      logger.info('Remove old transaction', llo({ txLog, dao: dao.address }))
+    }
   },
 
   stop: async () => {},
