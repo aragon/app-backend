31366645c6b0eccad6b884ea59e068b27e33cc39
cristianizzo <cristianizzo@me.com>
2025-03-01 00:39:49 +0700

    rabbitmq

--- tools/manualSyncProposals.ts
+++ tools/manualSyncProposals.ts
@@ -1,81 +1,28 @@
-import { EnumConnection, IPluginInterfaceType, type IService, NetworksEnum } from '@types'
+import { EnumConnection, type IService } from '@types'
 import { Models } from '@dbModels'
-import DBCrawler from '@models/utils/crawler'
-import type Plugin from '@models/schema/plugin'
-import Web3Helper from '@helpers/web3'
-import logger from '@logger'
-import { ProposalHandler } from '@handlers/proposalHandler'
+import GovernanceErc20Helper from '@helpers/governanceErc20'
 
-const llo = logger.logMeta.bind(null, { service: 'Tools: ManualSyncProposals' })
-
-export const ManualSyncProposals: IService = {
+export const ToolsManualSyncProposals: IService = {
   NEED_CONNECTIONS: [EnumConnection.MONGODB, EnumConnection.BLOCKCHAIN],
 
   start: async () => {
-    const data: any = []
-    let countWrongData = 0
-    const dbCrawler = new DBCrawler({
-      model: Models.Plugin,
-      onDocument: async (doc: Plugin) => {
-        // doc.build == 4 - 5 -> findIncrementalId
-        // doc.build < 4 getProposalCount
-
-        let onChainCount: number = 0
-        if (Number(doc.build) < 4) {
-          const count = await Web3Helper.getProposalCount(doc.address, doc.network)
-          onChainCount = Number(count)
-        } else {
-          onChainCount = await ProposalHandler.findIncrementalId({
-            pluginAddress: doc.address,
-            network: doc.network,
-            proposalIndex: '8912371293810298312908309182309123128038120312093',
-          })
-        }
-
-        const onDbCount = await Models.Proposal.countDocuments({ pluginAddress: doc.address, network: doc.network })
-
-        const isSame = onChainCount === onDbCount
-
-        if (!isSame) {
-          countWrongData++
-
-          data.push({
-            pluginAddress: doc.address,
-            network: doc.network,
-            onChainCount: onChainCount.toString(),
-            onDbCount: onDbCount.toString(),
-          })
-
-          logger.verbose(
-            'Fucked sync',
-            llo({
-              pluginAddress: doc.address,
-              network: doc.network,
-              onChainCount,
-              onDbCount,
-            }),
-          )
-        }
-      },
-      onError: (error: any, document: any) => {
-        logger.error('Error ManualSyncProposals', { document, error })
-      },
-      where: {
-        network: { $in: [NetworksEnum.ethereumMainnet, NetworksEnum.polygonMainnet] },
-        interfaceType: { $in: [IPluginInterfaceType.tokenVoting, IPluginInterfaceType.multisig] },
-      },
-      batchSize: 1000,
-      concurrency: 500,
+    const proposals = await Models.Proposal.find({
+      'snapshot.totalSupply': '0',
     })
 
-    await dbCrawler.crawl()
+    for (const proposal of proposals) {
+      const pastTotalSupply = await GovernanceErc20Helper.getPastTotalSupply(
+        proposal.blockNumber,
+        proposal.settings.tokenAddress,
+        proposal.network,
+      )
 
-    logger.info('Total wrong data', llo({ countWrongData, data }))
-
-    logger.info('END', llo())
+      proposal.snapshot.totalSupply = pastTotalSupply.toString()
+      await proposal.save()
+    }
   },
 
   stop: async () => {},
 }
 
-export default ManualSyncProposals
+export default ToolsManualSyncProposals
