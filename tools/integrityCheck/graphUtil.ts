96dad5e399036d555b4f01e82918dcfa2f82d015
ccir105 <sisnet2010@gmail.com>
2025-03-03 02:03:19 +0545

    Feat/integrity test (#369)
    * transaction indexing checking api
    
    * delegate returned from on-chain
    
    * format fix
    
    * integrity test tools
    
    * format:Fix
    
    * format:Fix
    
    * aragon-plugins instances
    
    ---------
    
    Co-authored-by: cristianizzo <cristianizzo@me.com>

---
+++ tools/integrityCheck/graphUtil.ts
@@ -0,0 +1,175 @@
+import axios from 'axios'
+import { type HexAddress, NetworksEnum } from '@types'
+import { ethers } from 'ethers'
+const URL = 'https://n-app-backend.aragon.org/graphql'
+
+const IntegrityUtil = {
+  networkToGraphNetwork: (network: NetworksEnum) => {
+    const networkMap = {
+      [NetworksEnum.polygonMainnet]: 'polygon',
+      [NetworksEnum.ethereumMainnet]: 'ethereum',
+      [NetworksEnum.ethereumSepolia]: 'sepolia',
+    }
+
+    return networkMap[network]
+  },
+
+  constructGraphUrl(network: NetworksEnum) {
+    const base = 'https://subgraph.satsuma-prod.com/qHR2wGfc5RLi6/aragon/osx-'
+    const suffix = '/version/1.4.2/api'
+    const networkMap = {
+      [NetworksEnum.polygonMainnet]: 'polygon',
+      [NetworksEnum.ethereumMainnet]: 'mainnet',
+      [NetworksEnum.ethereumSepolia]: 'sepolia',
+    }
+
+    return `${base}${networkMap[network]}${suffix}`
+  },
+
+  async getMembersFromGraphOfToken(network: NetworksEnum, tokenAddress: HexAddress) {
+    const query = `
+      query Holders($network: Network!, $tokenAddress: String!) {
+        holders(network: $network, tokenAddress: $tokenAddress) {
+          contractAddress
+          contractDecimals
+          contractName
+          contractTickerSymbol
+          hasMore
+          holders {
+            address
+            balance
+            delegates
+            votes
+          }
+          logoUrl
+          network
+          supportsErc
+          totalHolders
+          totalSupply
+          updatedAt
+        }
+      }
+    `
+
+    const variables = {
+      network: IntegrityUtil.networkToGraphNetwork(network),
+      tokenAddress: tokenAddress.toLowerCase(),
+    }
+
+    return await axios.post(URL, { query, variables })
+  },
+
+  async getMultisigBasedMembersFromGraph(network: NetworksEnum, pluginAddress: HexAddress) {
+    const url = IntegrityUtil.constructGraphUrl(network)
+
+    const query = `
+      query MultisigMembers($where: MultisigApprover_filter!) {
+        multisigApprovers(
+          where: $where
+        ) {
+          address
+          isActive
+        }
+      }
+    `
+
+    const variables = {
+      where: { plugin: pluginAddress.toLowerCase() },
+    }
+
+    return await axios.post(url, { operationName: 'MultisigMembers', query, variables })
+  },
+
+  async getProposalCountForTokenVoting(network: NetworksEnum, plugin: HexAddress) {
+    const url = IntegrityUtil.constructGraphUrl(network)
+
+    const query = `
+      query TokenVotingProposalCount($pluginAddress: ID!) {
+        tokenVotingPlugin(id: $pluginAddress) {
+          proposalCount
+        }
+      }
+    `
+
+    const variables = {
+      pluginAddress: plugin.toLowerCase(),
+    }
+
+    return await axios.post(url, { query, variables })
+  },
+
+  async getProposalCountForMultisigVoting(network: NetworksEnum, plugin: HexAddress) {
+    const url = IntegrityUtil.constructGraphUrl(network)
+
+    const query = `
+      query MultisigProposalCount($pluginAddress: ID!) {
+        multisigPlugin(id: $pluginAddress) {
+          proposalCount
+        }
+     }`
+
+    const variables = {
+      pluginAddress: plugin.toLowerCase(),
+    }
+
+    return await axios.post(url, { query, variables })
+  },
+
+  async getMultisigProposalInfoFromGraph(network: NetworksEnum, plugin: HexAddress, proposalId: string) {
+    const query = `
+      query MultisigProposal($proposalId: ID!) {
+        multisigProposal(id: $proposalId) {
+          id
+          minApprovals
+          executionTxHash
+          executed
+          approvalReached
+          isSignaling
+          approvals(first: 1000) {
+            id
+            approver {
+              address
+            }
+          }
+        }
+      }
+    `
+
+    const variables = {
+      proposalId: `${plugin.toLowerCase()}_${ethers.AbiCoder.defaultAbiCoder().encode(['uint256'], [proposalId])}`,
+    }
+
+    const url = IntegrityUtil.constructGraphUrl(network)
+    return await axios.post(url, { query, variables, operationName: 'MultisigProposal' })
+  },
+
+  async getTokenVotingProposalInfoFromGraph(network: NetworksEnum, plugin: HexAddress, proposalId: string) {
+    const query = `
+      query TokenVotingProposal($proposalId: ID!) {
+        tokenVotingProposal(id: $proposalId) {
+          id
+          voters(first: 1000) {
+            voter {
+              address
+            }
+            voteReplaced
+            voteOption
+            votingPower
+          }
+          totalVotingPower
+          minVotingPower
+        }
+      }
+    `
+
+    const variables = {
+      proposalId: `${plugin.toLowerCase()}_${ethers.AbiCoder.defaultAbiCoder().encode(['uint256'], [proposalId])}`,
+    }
+
+    const url = IntegrityUtil.constructGraphUrl(network)
+
+    return await axios.post(url, { query, variables, operationName: 'TokenVotingProposal' })
+  },
+}
+
+export default IntegrityUtil
---
+++ tools/integrityCheck/proposalCheck.ts
@@ -0,0 +1,173 @@
+import { EnumConnection, IPluginInterfaceType, IPluginStatus } from '@types'
+import { NetworkHelper } from '@helpers/network'
+import DBCrawler from '@models/utils/crawler'
+import { Models } from '@dbModels'
+import logger from '@logger'
+import IntegrityUtil from '@tools/integrityCheck/graphUtil'
+import Utils from '@helpers/utils'
+
+const llo = logger.logMeta.bind(null, { service: 'tool:IntegrityToolProposalCheck' })
+export const IntegrityToolProposalCheck: any = {
+  NEED_CONNECTIONS: [EnumConnection.MONGODB, EnumConnection.BLOCKCHAIN],
+  BAD_VOTE_PROPOSAL: [],
+  BAD_PROPOSAL_PLUGIN: [],
+  ERROR_PROPOSAL_PLUGIN: [],
+  start: async () => {
+    const networks = NetworkHelper.supportedNetworks()
+    await Promise.all(
+      networks.map(async ({ networkName }) => {
+        const dbCrawler = new DBCrawler({
+          model: Models.Plugin,
+          where: {
+            network: networkName,
+            status: IPluginStatus.installed,
+          },
+          onDocument: IntegrityToolProposalCheck.onDocument,
+          onError: (_error: any, document: any) => {
+            IntegrityToolProposalCheck.ERROR_PROPOSAL_PLUGIN.push({
+              network: document.network,
+              address: document.address,
+            })
+            logger.error('IntegrityToolProposalCheck error', { document: document.address })
+          },
+          batchSize: 50,
+          concurrency: 5,
+        })
+
+        await dbCrawler.crawl()
+      }),
+    )
+
+    logger.info(
+      'IntegrityToolProposalCheck finished',
+      llo({
+        BAD_VOTE_PROPOSAL: IntegrityToolProposalCheck.BAD_VOTE_PROPOSAL,
+        BAD_PROPOSAL_PLUGIN: IntegrityToolProposalCheck.BAD_PROPOSAL_PLUGIN,
+        ERROR_PROPOSAL_PLUGIN: IntegrityToolProposalCheck.ERROR_PROPOSAL_PLUGIN,
+      }),
+    )
+  },
+
+  handleResult: async (plugin: any, type: any, graphCount: number, dbCount: number) => {
+    if (graphCount !== dbCount) {
+      IntegrityToolProposalCheck.BAD_PROPOSAL_PLUGIN.push({
+        network: plugin.network,
+        type,
+        address: plugin.address,
+      })
+      logger.error(`❌ ${type} mismatch for ${plugin.address}`, llo({ graphCount, dbCount }))
+    } else {
+      logger.info(`✅ ${type} match for ${plugin.address}`, llo({ graphCount, dbCount }))
+    }
+
+    const dbProposals = await Models.Proposal.find({
+      pluginAddress: plugin.address,
+      network: plugin.network,
+    })
+
+    const proposalIds = dbProposals.map((proposal: any) => Number(proposal.proposalIndex))
+
+    for (const proposal of proposalIds) {
+      await Utils.wait(100)
+      if (type === 'multisig') {
+        const graphResult = await IntegrityUtil.getMultisigProposalInfoFromGraph(
+          plugin.network,
+          plugin.address,
+          proposal,
+        )
+        const graphProposalVotes = graphResult.data.data.multisigProposal.approvals
+        const dbProposalVotes = await Models.Vote.countDocuments({
+          proposalIndex: `${proposal}`,
+          pluginAddress: plugin.address,
+          network: plugin.network,
+        })
+
+        if (graphProposalVotes.length !== dbProposalVotes) {
+          IntegrityToolProposalCheck.BAD_VOTE_PROPOSAL.push({
+            network: plugin.network,
+            plugin: plugin.address,
+            proposalIndex: proposal,
+          })
+          logger.error(`❌ Proposal votes mismatch for ${plugin.address} and proposal ${proposal}`)
+        } else {
+          logger.info(`✅ Proposal votes match for ${plugin.address} and proposal ${proposal}`)
+        }
+
+        continue
+      }
+
+      const graphResult = await IntegrityUtil.getTokenVotingProposalInfoFromGraph(
+        plugin.network,
+        plugin.address,
+        proposal,
+      )
+      const graphProposalVotes = graphResult.data.data.tokenVotingProposal.voters
+      const dbProposalVotes = await Models.Vote.countDocuments({
+        proposalIndex: `${proposal}`,
+        pluginAddress: plugin.address,
+        network: plugin.network,
+      })
+
+      if (graphProposalVotes.length !== dbProposalVotes) {
+        logger.error(`❌ Proposal votes mismatch for ${plugin.address} and proposal ${proposal}`)
+        IntegrityToolProposalCheck.BAD_VOTE_PROPOSAL.push({
+          network: plugin.network,
+          plugin: plugin.address,
+          proposalIndex: proposal,
+        })
+      } else {
+        logger.info(`✅ Proposal votes match for ${plugin.address} and proposal ${proposal}`)
+      }
+    }
+  },
+
+  checkForMultisig: async (plugin: any) => {
+    const graphResult = await IntegrityUtil.getProposalCountForMultisigVoting(plugin.network, plugin.address)
+    const graphProposalCount = graphResult.data.data.multisigPlugin.proposalCount
+    const dbProposalCount = await Models.Proposal.countDocuments({
+      pluginAddress: plugin.address,
+      network: plugin.network,
+    })
+
+    await IntegrityToolProposalCheck.handleResult(
+      plugin,
+      'multisig',
+      Number(graphProposalCount),
+      Number(dbProposalCount),
+    )
+  },
+
+  checkForTokenVoting: async (plugin: any) => {
+    const graphResult = await IntegrityUtil.getProposalCountForTokenVoting(plugin.network, plugin.address)
+    const graphProposalCount = graphResult.data.data.tokenVotingPlugin.proposalCount
+    const dbProposalCount = await Models.Proposal.countDocuments({
+      pluginAddress: plugin.address,
+      network: plugin.network,
+    })
+
+    await IntegrityToolProposalCheck.handleResult(
+      plugin,
+      'tokenVoting',
+      Number(graphProposalCount),
+      Number(dbProposalCount),
+    )
+  },
+
+  onDocument: async (plugin: any) => {
+    await Utils.wait(100)
+
+    switch (plugin.interfaceType) {
+      case IPluginInterfaceType.multisig:
+        await IntegrityToolProposalCheck.checkForMultisig(plugin)
+        break
+      case IPluginInterfaceType.tokenVoting:
+        await IntegrityToolProposalCheck.checkForTokenVoting(plugin)
+        break
+      default:
+    }
+  },
+
+  stop: async () => {},
+}
+
+export default IntegrityToolProposalCheck
