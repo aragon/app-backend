09826da54e66d43a641836736a35cb4f935fe461
sishir <sisnet2010@gmail.com>
2025-03-05 16:26:22 +0545

    token issue debug

--- src/services/aragon-dao/tokenInfo.ts
+++ src/services/aragon-dao/tokenInfo.ts
@@ -2,9 +2,8 @@ import { type NetworksEnum } from '@types'
 import { Models } from '@dbModels'
 import logger from '@logger'
 import Utils from '@helpers/utils'
-import DbTx from '@modules/dbTx'
 import TokenDetailProvider from '@providers/tokenDetailProvider/providerFactory'
-import { type SaveOptions } from 'mongoose'
+import DbOperations from '@models/utils/dbOperations'
 
 const llo = logger.logMeta.bind(null, { service: 'tokenInfo' })
 
@@ -15,60 +14,56 @@ interface PollingOptions {
 
 export const TokenDetailFetcherWithRetry = {
   async update(tokenAddress: string, network: NetworksEnum): Promise<void> {
-    await DbTx.executeTxFn(async ({ session }) => {
-      try {
-        const pluginAttachedToken = await Models.Plugin.findByTokenAddress(tokenAddress, network, { session })
+    try {
+      const pluginAttachedToken = await Models.Plugin.findByTokenAddress(tokenAddress, network)
 
-        if (!pluginAttachedToken) {
-          logger.warn('Token not okay for pooling', llo({ tokenAddress, network }))
-          return
-        }
-
-        logger.verbose('Updating Token with pooling', llo({ tokenAddress, network }))
-
-        let tokenInfo: { tokenDb: any; tokenDetails: any } | undefined = { tokenDb: null, tokenDetails: null }
-
-        try {
-          tokenInfo = await TokenDetailFetcherWithRetry.pollWithRetry(tokenAddress, network, session)
-        } catch (_) {
-          //
-        }
-
-        if (tokenInfo?.tokenDb) {
-          await tokenInfo.tokenDb.update(
-            {
-              name: tokenInfo.tokenDetails.name,
-              symbol: tokenInfo.tokenDetails.symbol,
-              totalSupply: tokenInfo.tokenDetails.totalSupply,
-              holders: tokenInfo.tokenDetails.totalHolders,
-              decimals: tokenInfo.tokenDetails.decimals,
-              priceUsd: tokenInfo.tokenDetails.priceUsd,
-            },
-            { session },
-          )
-
-          await session.commitTransaction()
-          await session.endSession()
-
-          logger.verbose('Updated Token with pooling', llo({ logId: tokenInfo?.tokenDb.id }))
-        }
-      } catch (error) {
-        logger.error('Error updating token metrics', llo({ tokenAddress, network, error }))
+      if (!pluginAttachedToken) {
+        logger.warn('Token not okay for pooling', llo({ tokenAddress, network }))
+        return
       }
-    })
+
+      logger.verbose('Updating Token with pooling', llo({ tokenAddress, network }))
+
+      let tokenInfo: { tokenDb: any; tokenDetails: any } | undefined = { tokenDb: null, tokenDetails: null }
+
+      try {
+        tokenInfo = await TokenDetailFetcherWithRetry.pollWithRetry(tokenAddress, network)
+      } catch (_) {
+        //
+      }
+
+      if (tokenInfo?.tokenDb) {
+        await DbOperations.updateDocument(
+          tokenInfo.tokenDb,
+          {
+            name: tokenInfo.tokenDetails.name,
+            symbol: tokenInfo.tokenDetails.symbol,
+            totalSupply: tokenInfo.tokenDetails.totalSupply,
+            holders: tokenInfo.tokenDetails.totalHolders,
+            decimals: tokenInfo.tokenDetails.decimals,
+            priceUsd: tokenInfo.tokenDetails.priceUsd,
+          },
+          { address: tokenAddress, network },
+          'Updated Token with pooling',
+          llo,
+        )
+        logger.verbose('Updated Token with pooling', llo({ logId: tokenInfo?.tokenDb.id }))
+      }
+    } catch (error) {
+      logger.error('Error updating token metrics', llo({ tokenAddress, network, error }))
+    }
   },
 
   async pollWithRetry(
     tokenAddress: string,
     network: NetworksEnum,
-    session: SaveOptions,
     options: PollingOptions = { intervalMs: 15000, timeoutMs: 300000 }, // interval 15 minute, timeout 5 minutes for safety
   ): Promise<{ tokenDb: any; tokenDetails: any } | undefined> {
     const { intervalMs, timeoutMs } = options
     const startTime = Date.now()
 
     while (Date.now() - startTime < timeoutMs) {
-      const tokenDb = await Models.Token.findOne({ address: tokenAddress, network }, null, { session })
+      const tokenDb = await Models.Token.findOne({ address: tokenAddress, network })
       if (tokenDb) {
         const tokenDetails = await TokenDetailProvider.fetchBasicTokenInfo(tokenDb)
         if (TokenDetailFetcherWithRetry.hasValidInfo(tokenDetails)) {
