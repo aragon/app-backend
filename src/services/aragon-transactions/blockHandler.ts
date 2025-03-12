f658a863c67e79d0be95771716e09fc297b21582
sishir <sisnet2010@gmail.com>
2025-03-12 13:52:45 +0545

    block interval was wrong

--- src/services/aragon-transactions/blockHandler.ts
+++ src/services/aragon-transactions/blockHandler.ts
@@ -3,6 +3,8 @@ import { EnumQueueName, type NetworksEnum } from '@types'
 import { Models } from '@dbModels'
 import RabbitMQHelper from '@helpers/rabbitMQ'
 import type Dao from '@models/schema/dao'
+import utils from '@helpers/utils'
+import config from '@config'
 
 const llo = logger.logMeta.bind(null, { service: 'service:aragon-transactions:BlockHandler' })
 
@@ -10,6 +12,9 @@ export const BlockHandler = {
   processReceiver: async (transactionHash: string, toAddresses: string[], network: NetworksEnum) => {
     const daos = await Models.Dao.find({ address: { $in: toAddresses }, network })
     if (!daos || daos.length === 0) return
+
+    // wait for confirmation blocks
+    await utils.wait(config.NODES[utils.networkToAragon(network)].INTERVAL_BLOCK_TIME * 1000)
 
     await Promise.all(
       daos.map(async (dao: any) => {
