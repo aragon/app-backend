90a5261093602c43355d515e5eb7d39c952fd522
cristianizzo <cristianizzo@me.com>
2025-03-14 03:59:50 +0700

    feat: increase instances

--- tools/refetchProposalMetrics.ts
+++ tools/refetchProposalMetrics.ts
@@ -13,6 +13,7 @@ export const RefetchProposalsMetrics: IService = {
       model: Models.Proposal,
       onDocument: async (proposal: Proposal) => {
         const plugin = await Models.Plugin.findByAddress(proposal.pluginAddress, proposal.network)
+        if (!plugin.isSupported) return
         if (plugin.interfaceType === IPluginInterfaceType.tokenVoting) {
           await ProposalMetrics.proposalTokenVotingMetrics({
             pluginAddress: proposal.pluginAddress,
