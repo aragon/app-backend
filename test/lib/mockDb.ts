0e46f99ef8c3516af4804532dfc651869bf8dd6d
sishir <sisnet2010@gmail.com>
2025-03-13 15:13:17 +0545

    feat(test): fixed mocha test to run fast

--- test/lib/mockDb.ts
+++ test/lib/mockDb.ts
@@ -17,7 +17,7 @@ const MockDB = {
 
   connect: async () => {
     await MockDB._connectMongoDB()
-    await MockDB.syncIndexesForAllModels()
+    // await MockDB.syncIndexesForAllModels()
   },
 
   drop: async () => {
@@ -56,11 +56,16 @@ const MockDB = {
       },
     })
 
+
+
     await MockDB.replSet.start()
     const uri = MockDB.replSet.getUri()
     await ModelProxy.setMongoModels()
 
     await mongoose.connect(uri, MockDB.mongoOptions)
+    mongoose.connection.on('connected', () => {
+      console.log(`Mongoose connected to ${uri}`);
+    });
     mongoose.set('debug', config.MONGO_DB.DEBUGGER)
     MockDB.mongoUri = uri
     return uri
