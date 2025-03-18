462ba3e30ca9035a2072dd5ce04e20905d223ded
cristianizzo <cristianizzo@me.com>
2025-03-18 17:37:05 +0700

    fix: alchemy transactions

--- src/services/aragon-dao/daoTransactions.ts
+++ src/services/aragon-dao/daoTransactions.ts
@@ -54,6 +54,8 @@ export const DaoTransactions = {
     ]
 
     switch (network) {
+      case NetworksEnum.ethereumSepolia:
+        return category.filter(cat => cat !== ITransactionCategory.Internal)
       case NetworksEnum.baseMainnet:
       case NetworksEnum.zksyncSepolia:
       case NetworksEnum.arbitrumMainnet:
