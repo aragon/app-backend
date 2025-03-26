2efcd5e1b755993f1e1929cf8b22943bd62be8e8
cristianizzo <cristianizzo@me.com>
2025-03-26 16:45:43 +0700

    test: format

--- test/unit-dep/rabbitmq/sender.ts
+++ test/unit-dep/rabbitmq/sender.ts
@@ -1,13 +1,13 @@
-import RabbitMQHelper from "@helpers/rabbitMQ";
-import {EnumQueueName} from "@types";
+import RabbitMQHelper from '@helpers/rabbitMQ'
+import { EnumQueueName } from '@types'
 
 const SenderRabbitMQ = {
-  sendMessage: async (queueName: string, data: {id: string, params: any}): Promise<void> => {
+  sendMessage: async (queueName: string, data: { id: string; params: any }): Promise<void> => {
     await RabbitMQHelper.sendMessage(queueName as any, {
       id: data.id,
-      params: data.params
+      params: data.params,
     })
-  }
+  },
 }
 
 export default SenderRabbitMQ
