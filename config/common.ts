f1f894dc56ae3b587e2ccc002f18d74ad2261645
cristianizzo <cristianizzo@me.com>
2025-04-02 18:17:37 +0700

    feat: remove websocket

--- config/common.ts
+++ config/common.ts
@@ -1,6 +1,5 @@
 import utils from '@helpers/utils'
 import { type IConfig, NetworksEnum, SupportedEnsNetworksEnum } from '@types'
-import BlockchainLogCrawler from '@modules/blockchainLogCrawler'
 
 const getConfigObject = (sourceConfig: Record<string, any>): IConfig => {
   return {
