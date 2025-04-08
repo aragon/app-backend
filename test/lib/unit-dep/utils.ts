c3ff386143fa0d56648885ece223e3e854036891
sishir <sisnet2010@gmail.com>
2025-04-08 07:37:29 +0545

    feat(peaq): fix contract sourcecode part when decoding

--- test/lib/unit-dep/utils.ts
+++ test/lib/unit-dep/utils.ts
@@ -123,6 +123,57 @@ const UnitDepUtils = {
 
     return parsedLogs
   },
+
+  registerRepoForPeaq: async () => {
+    const repos = [
+      {
+        id: 'peaq-mainnet-0xaf2fff600394b1d37fb3ee8c4db3cf42e67b770bc942c84aef83d007026f72a3-0-9',
+        transactionHash: '0xaf2fff600394b1d37fb3ee8c4db3cf42e67b770bc942c84aef83d007026f72a3',
+        transactionIndex: 0,
+        logIndex: 9,
+        blockNumber: 4032542,
+        blockTimestamp: 1740069708,
+        network: NetworksEnum.peaqMainnet,
+        subdomain: 'multisig',
+        pluginRepo: '0x83a977d564349586936f17D9536b2c5702B4Fe20',
+      },
+      {
+        id: 'peaq-mainnet-0x9f98e40ad433af72937c4129f90a57408459f647494f292df9992f92ac19cec2-0-9',
+        transactionHash: '0x9f98e40ad433af72937c4129f90a57408459f647494f292df9992f92ac19cec2',
+        transactionIndex: 0,
+        logIndex: 9,
+        blockNumber: 4032643,
+        blockTimestamp: 1740070338,
+        network: 'peaq-mainnet',
+        subdomain: NetworksEnum.peaqMainnet,
+        pluginRepo: '0xFBFbE98845B4E2751a8A004B5A1759e3A278FC68',
+      },
+      {
+        id: 'peaq-mainnet-0xeaa8afef694113dbc91aafc4c8ab072768a752150926a6fd42271bbaaa70a901-1-10',
+        transactionHash: '0xeaa8afef694113dbc91aafc4c8ab072768a752150926a6fd42271bbaaa70a901',
+        transactionIndex: 1,
+        logIndex: 10,
+        blockNumber: 4032714,
+        blockTimestamp: 1740070770,
+        network: NetworksEnum.peaqMainnet,
+        subdomain: 'admin',
+        pluginRepo: '0x86C87Aa7C09a447048adf4197fec7C12eF62A07F',
+      },
+      {
+        id: 'peaq-mainnet-0x0fe819a1a087dd5faa04f0bad2e381da56ecbb15c5afde5b16f0d59943990d8b-0-9',
+        transactionHash: '0x0fe819a1a087dd5faa04f0bad2e381da56ecbb15c5afde5b16f0d59943990d8b',
+        transactionIndex: 0,
+        logIndex: 9,
+        blockNumber: 4032890,
+        blockTimestamp: 1740071868,
+        network: NetworksEnum.peaqMainnet,
+        subdomain: 'spp',
+        pluginRepo: '0x2784e9500f8f60C1267e819f216682a88A37d56D',
+      },
+    ]
+
+    await Promise.all(repos.map(async repo => await Models.PluginRepo.create(repo)))
+  },
 }
 
 export default UnitDepUtils
