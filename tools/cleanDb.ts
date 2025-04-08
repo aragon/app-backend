0ed4366779974f8afd3db87e8d36a9f0a71bfc11
sishir <sisnet2010@gmail.com>
2025-04-08 11:13:58 +0545

    feat(peaq): fix test

--- tools/cleanDb.ts
+++ tools/cleanDb.ts
@@ -16,7 +16,7 @@ export const ToolsCleanDb: IService = {
         // Ensure the model has a deleteMany method
         const dbModel = model as any
         if (typeof dbModel.deleteMany === 'function') {
-          const result = await dbModel.deleteMany({ network: networkToDelete, blockNumber: { $lt: 0 } })
+          const result = await dbModel.deleteMany({ network: networkToDelete })
           logger.verbose(
             `Deleted ${result.deletedCount} documents from ${modelName} where network is ${networkToDelete}`,
             llo(),
