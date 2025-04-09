0d33e1b623663d818523f0898715377dd554afbf
sishir <sisnet2010@gmail.com>
2025-04-09 15:40:55 +0545

    feat(peaq): fix prop

--- tools/manualTrigger.ts
+++ tools/manualTrigger.ts
@@ -4,8 +4,6 @@ import BlockchainLogCrawler from '@modules/blockchainLogCrawler'
 import utils from '@helpers/utils'
 import configIndexer from '@indexer/configIndexer'
 import logger from '@logger'
-import { Models } from '@dbModels'
-import { ProposalHandler } from '@handlers/proposalHandler'
 
 const llo = logger.logMeta.bind(null, { service: 'service:IndexerService' })
 
@@ -14,28 +12,23 @@ export const ToolsManualTrigger: IService = {
 
   start: async () => {
     await ProviderModule.connectToAllNetworks()
-    const network = NetworksEnum.polygonMainnet
+    const network = NetworksEnum.ethereumSepolia
 
-    const address = '0x907bAab1aBF2D7a07DE1f1958a1afdDca28ad0E0'
-    const proposals = await Models.Proposal.find({
-      pluginAddress: address,
+    const fromBlock = 7149126
+    const toBlock = 'latest'
+    const configLogs = utils.filterArrayByProperty(configIndexer, 'enableHistorical')
+
+    const logCrawler = new BlockchainLogCrawler({
+      fromBlock,
+      toBlock,
+      events: configLogs,
       network,
-    }).sort({ incrementalId: 1 })
-    if (proposals.length === 0) {
-      llo('No proposals found')
-      return
-    }
+      onError: async (error: any) => logger.error('Error Indexer', llo(error)),
+      logService: `indexer-${network}`,
+      stopOnError: true,
+    })
 
-    for (const proposal of proposals) {
-      const incrementalId = await ProposalHandler.findIncrementalId(proposal)
-      if (incrementalId !== proposal.incrementalId) {
-        logger.info('Incremental ID mismatch', llo({ incrementalId, proposal }))
-      } else {
-        logger.info('Incremental ID match', llo({ incrementalId, proposal: proposal.id }))
-      }
-    }
-
-    logger.info('Proposals Finished', llo({ proposalsCount: proposals.length }))
+    await logCrawler.crawl()
   },
 
   stop: async () => {},
