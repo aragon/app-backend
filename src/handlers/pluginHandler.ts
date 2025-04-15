ae87c137997f6a519a5761683dd21069c1ae26b7
ccir105 <sisnet2010@gmail.com>
2025-04-15 23:06:36 +0545

    feat(peaq): debug peaq network (#498)
    * feat(peaq): debug peaq network
    
    * feat(peaq): debug peaq network
    
    * feat(peaq): debug peaq network

--- src/handlers/pluginHandler.ts
+++ src/handlers/pluginHandler.ts
@@ -8,7 +8,7 @@ import {
   IPluginRawStatus,
   IPluginStatus,
   type IQueryGetPlugin,
-  type NetworksEnum,
+  NetworksEnum,
 } from '@types'
 import type LogPluginSetupProcessor from '@models/schema/logPluginSetupProcessor'
 import type Plugin from '@models/schema/plugin'
@@ -20,6 +20,7 @@ import PluginDetector from '@helpers/pluginDetector'
 import { PluginSetupProcessor } from '@artifacts/pluginSetupProcessor'
 import { PluginSlug } from '@helpers/pluginSlug'
 import DbTx from '@modules/dbTx'
+import utils from '@helpers/utils'
 
 const llo = logger.logMeta.bind(null, { service: 'handlers:PluginHandler' })
 
@@ -278,7 +279,16 @@ export const PluginHandler = {
       tokenAddress: plugin.tokenAddress,
     }
 
-    const pluginInfo = await PluginDetector.detectPluginType(plugin.address, plugin.network)
+    let pluginInfo = await PluginDetector.detectPluginType(plugin.address, plugin.network)
+    if (
+      pluginInfo.type === IPluginInterfaceType.unknown &&
+      plugin.subdomain &&
+      plugin.network === NetworksEnum.peaqMainnet
+    ) {
+      await utils.wait(1000)
+      pluginInfo = await PluginDetector.detectPluginType(plugin.address, plugin.network)
+    }
+
     document.interfaceType = pluginInfo?.type
 
     if (document.interfaceType === IPluginInterfaceType.tokenVoting) {
