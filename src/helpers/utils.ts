0c64bd3125f68f2997d82f179b5a8d33a08e97c9
cristianizzo <cristianizzo@me.com>
2025-04-22 22:11:26 +0700

    test: blockchain log crawler implement offset to block

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -23,15 +23,6 @@ const Utils = {
 
   networkToAragon: (network: NetworksEnum) => Utils.aragonNetworkMap[network],
 
-  getLastBlockStrategy(network: NetworksEnum): string {
-    switch (network) {
-      case NetworksEnum.peaqMainnet:
-        return 'finalized'
-      default:
-        return 'latest'
-    }
-  },
-
   validateString(input: string | null | undefined): string | null {
     if (typeof input === 'string' && input.trim() !== '') {
       return input
