4713092294f489b03c14d6194271f9411c6b33fa
cristianizzo <cristianizzo@me.com>
2025-04-23 17:20:07 +0700

    test: unit test

--- test/unit/services/aragon-api/routers/dao.spec.ts
+++ test/unit/services/aragon-api/routers/dao.spec.ts
@@ -52,6 +52,43 @@ describe('Router: Dao', () => {
       expect(stubCtrl.args[0][1]).to.deep.eq(filterParams)
     })
 
+    it('Should get dao with pagination - all string params', async () => {
+      const filterParams = {
+        networks: 'ethereum-mainnet,ethereum-sepolia',
+        address: '0xf2d594F3C93C19D7B1a6F15B5489FFcE4B01f7dA',
+        pluginAddress: '0xf2d594F3C93C19D7B1a6F15B5489FFcE4B01f7dA',
+      }
+      const paginationParams = {
+        pageSize: 10,
+        page: 1,
+        order: 'asc',
+        sort: 'createdAt',
+      }
+
+      const stubCtrl = sandbox.stub(DaoController, 'getDaosWithPagination').returns(true as any)
+
+      const ctx: any = {
+        query: { ...filterParams, ...paginationParams },
+      }
+
+      await DaoRouter.getWithPagination(ctx)
+
+      expect(ctx.body).to.eq(true)
+      expect(stubCtrl.calledOnce).to.be.true
+
+      const missingParams = {
+        endDateProp: undefined,
+        startDateProp: undefined,
+        endDate: undefined,
+        startDate: undefined,
+        search: undefined,
+      }
+      expect(stubCtrl.args[0][0]).to.deep.eq({ ...paginationParams, ...missingParams })
+      expect(stubCtrl.args[0][1]?.networks?.toString()).to.eq(filterParams.networks)
+      expect(stubCtrl.args[0][1]?.address).to.eq(filterParams.address)
+      expect(stubCtrl.args[0][1]?.pluginAddress).to.eq(filterParams.pluginAddress)
+    })
+
     it('Should get dao with pagination - missing pagination params', async () => {
       const filterParams = {
         networks: NetworksEnum.ethereumMainnet,
