951df51f60e803453f2dd66a402e029b88c3d3ef
cristianizzo <cristianizzo@me.com>
2025-04-30 18:08:45 +0700

    chore: fix hotfix whitelist error rpc

--- src/modules/blockchainLogCrawler.ts
+++ src/modules/blockchainLogCrawler.ts
@@ -671,6 +671,7 @@ class BlockchainLogCrawler {
     const messages = [
       'The query timed out',
       'timeout',
+      'th_getLogs is limited to a 10,000 rang',
       'Response size is larger than 150MB limit',
       'Log response size exceeded',
       'Consider reducing your block range',
