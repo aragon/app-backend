c1ded3cc3298fcfe8fbb18dc4bcdaaef130fa599
sishirpokhrel <sisnet2010@gmail.com>
2025-05-08 12:39:24 +0545

    feat(lint): fix broken manual test lint issue

--- test/manual/token.spec.ts
+++ test/manual/token.spec.ts
@@ -8,6 +8,8 @@ import { RateModule } from '@modules/rates'
 import CovalentHelper from '@helpers/covalent'
 import { Models } from '@dbModels'
 import { expect } from 'chai'
+import ProxyProvider from '@modules/proxyProvider'
+import ProxyWeb3Provider from '@modules/proxyProvider'
 
 describe('Manual: Token', () => {
   let sandbox: SinonSandbox
@@ -41,7 +43,7 @@ describe('Manual: Token', () => {
       totalHolders: 1,
       totalSupply: '1',
     } as any)
-    sandbox.stub(ProxyToken, 'getContractCreationInfo').resolves({
+    sandbox.stub(ProxyWeb3Provider, 'fetchContractCreation').resolves({
       blockNumber: 100,
       transactionHash: '0x000',
       address: tokenAddress,
