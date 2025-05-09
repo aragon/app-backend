371463c67426993f583f3fbf5057452cb33a07f0
ccir105 <sisnet2010@gmail.com>
2025-05-09 14:39:05 +0545

    feat(memberInfo): canCreateProposal check on chain (#560)
    * feat(memberInfo): implement canCreateProposal logic and integrate RabbitMQ processing
    
    * feat(memberInfo): format:fix
    
    * feat(memberInfo): format:fix
    
    * feat(memberInfo): update job parameter from userAddress to memberAddress
    
    * feat(memberInfo): test fixed
    
    ---------
    
    Co-authored-by: CryptoCrash <cristianizzo@me.com>

--- src/services/aragon-dao/memberInfo.ts
+++ src/services/aragon-dao/memberInfo.ts
@@ -1,8 +1,10 @@
 import Web3Helper from '@helpers/web3'
 import GovernanceErc20Helper from '@helpers/governanceErc20'
-import { IPluginInterfaceType, type NetworksEnum } from '@types'
+import { type HexAddress, IPluginInterfaceType, type NetworksEnum } from '@types'
 import { ProxyToken } from '@modules/proxyToken'
 import { Models } from '@dbModels'
+import type Plugin from '@models/schema/plugin'
+import type PluginSetting from '@models/schema/setting'
 
 export const MemberInfo = {
   getByTokenAddress: async (
@@ -52,5 +54,53 @@ export const MemberInfo = {
     } catch (e) {
       return response
     }
+  },
+
+  canCreateProposal: async (pluginAddress: HexAddress, memberAddress: HexAddress, network: NetworksEnum) => {
+    try {
+      const plugin = await Models.Plugin.findByAddress(pluginAddress, network)
+      if (!plugin) {
+        return false
+      }
+
+      const settings = await Models.Setting.findActive({
+        daoAddress: plugin.daoAddress,
+        pluginAddress: plugin.address,
+        network: plugin.network,
+      })
+
+      switch (plugin.interfaceType) {
+        case IPluginInterfaceType.tokenVoting:
+          return await MemberInfo._checkForTokenVoting(plugin, settings, memberAddress)
+        case IPluginInterfaceType.multisig:
+          return await MemberInfo._checkForMultiSig(plugin, settings, memberAddress)
+        case IPluginInterfaceType.admin:
+          return await MemberInfo._checkForAdmin(plugin, settings, memberAddress)
+        default:
+          return false
+      }
+    } catch (e) {
+      return false
+    }
+  },
+
+  _checkForTokenVoting: async (plugin: Plugin, setting: PluginSetting, memberAddress: HexAddress) => {
+    if (!setting || !plugin.tokenAddress) return false
+    const votingPower = await GovernanceErc20Helper.getVotes(memberAddress, plugin.tokenAddress, plugin.network)
+    return Number(votingPower) > 0 && Number(votingPower) >= Number(setting.minParticipation)
+  },
+  _checkForMultiSig: async (plugin: Plugin, setting: PluginSetting, memberAddress: HexAddress) => {
+    if (!setting) return false
+
+    return setting?.onlyListed ? await Web3Helper.isMember(plugin.address, memberAddress, plugin.network) : true
+  },
+  _checkForAdmin: async (plugin: Plugin, _setting: PluginSetting, memberAddress: HexAddress) => {
+    const daoMemberMapping = await Models.DaoMemberMapping.findOne({
+      daoAddress: plugin.daoAddress,
+      memberAddress,
+      network: plugin.network,
+    })
+
+    return !!daoMemberMapping
   },
 }
