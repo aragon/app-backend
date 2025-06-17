327bb2567f28b78b9ba170c9755042d32fce369b
ccir <harryburger105@gmail.com>
2025-06-17 11:19:39 +0545

    feat(governance): implement getPastVotesWithRetry and enhance voting power retrieval

--- test/unit-dep/commonIntegrationTest.spec.ts
+++ test/unit-dep/commonIntegrationTest.spec.ts
@@ -2,9 +2,11 @@ import * as sinon from 'sinon'
 import BlockchainLogCrawler from '@modules/blockchainLogCrawler'
 import { NetworksEnum } from '@types'
 import configIndexer from '@indexer/configIndexer'
-import logger from '@logger'
 import Web3Helper from '@helpers/web3'
 import { expect } from 'chai'
+import GovernanceErc20Helper from '@helpers/governanceErc20'
+import logger from '@logger'
+import Web3BatchHelper from '@helpers/web3BatchHelper'
 
 describe('Basic Integer Test', () => {
   let sandbox: sinon.SinonSandbox
@@ -17,12 +19,13 @@ describe('Basic Integer Test', () => {
     sandbox.restore()
   })
 
-  it('should parse native token deposited event with crawler', async () => {
+  it('should parse native token deposited event with crawler', async function () {
+    this.timeout(1000000)
     const crawler = new BlockchainLogCrawler({
       network: NetworksEnum.arbitrumMainnet,
       logService: null,
       events: configIndexer,
-      onError: async (error: any) => logger.error('Error Indexer'),
+      onError: async (_error: any) => logger.error('Error Indexer'),
       stopOnError: true,
     })
 
@@ -42,5 +45,37 @@ describe('Basic Integer Test', () => {
 
     expect(!!formatted.event).to.be.true
     expect(formatted.event.name).to.equal('NativeTokenDeposited')
+  })
+
+  it('should investigate error getting past votes', async () => {
+    const memberAddress = '0xe858f73188F9f85748550A8521822a9Bc741a676'
+    const network = NetworksEnum.polygonMainnet
+    const tokenAddress = '0x46122a25470728244fB45Fe3955F965e6ccf8fB8'
+    const blockNumber = 72863483
+    const blockTimestamp = 1750129619
+
+    const originalEthCall = Web3BatchHelper.ethCall
+    const forcedBlockNumberHex = '0x' + blockNumber.toString(16)
+
+    sandbox.stub(Web3BatchHelper, 'ethCall').callsFake(async (calls, networkParam, blockTag?) => {
+      return originalEthCall.call(Web3BatchHelper, calls, networkParam, forcedBlockNumberHex)
+    })
+
+    const pastVotes = await GovernanceErc20Helper.getPastVotes(
+      memberAddress,
+      tokenAddress,
+      blockNumber,
+      blockTimestamp,
+      network,
+    )
+
+    logger.info('Past Votes:', {
+      pastVotes: pastVotes,
+    })
+
+    const ethCallStub = Web3BatchHelper.ethCall as sinon.SinonStub
+    expect(ethCallStub.callCount).to.be.eq(2)
+    expect(ethCallStub.firstCall.args[2]).to.be.eq(forcedBlockNumberHex)
+    expect(ethCallStub.secondCall.args[2]).to.be.eq(forcedBlockNumberHex)
   })
 })
