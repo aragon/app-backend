7589bd62f83351c7b14e278dadb25e758e4c4d2c
ccir <harryburger105@gmail.com>
2025-06-24 17:50:02 +0545

    fix(spp): fix of parent proposals during any error on spp pair

--- tools/fixSppPair.ts
+++ tools/fixSppPair.ts
@@ -1,4 +1,4 @@
-import { EnumConnection, type IService, NetworksEnum } from '@types'
+import { EnumConnection, type IService, type NetworksEnum } from '@types'
 import logger from '@logger'
 import { Models } from '@dbModels'
 import { ProposalHandler } from '@handlers/proposalHandler'
@@ -9,30 +9,46 @@ export const FixSppPair: IService = {
   NEED_CONNECTIONS: [EnumConnection.MONGODB, EnumConnection.BLOCKCHAIN],
 
   start: async () => {
-    const proposalParent = await Models.Proposal.findOne({
-      daoAddress: '0xBe31BC9278e4745d9D04F4A9113B71Db3Bdc7E43',
+    const parentProposals = await Models.Proposal.find({
       pluginSubdomain: 'spp',
       isSubProposal: false,
-      network: NetworksEnum.cornMainnet,
+      stageIndex: { $exists: false },
     })
 
-    if (!proposalParent) {
-      logger.error('Proposal parent not found', llo())
+    if (!parentProposals || parentProposals.length === 0) {
+      logger.info('No broken parent proposals found for SPP', llo())
       return
     }
 
-    const info = {
-      transactionHash: proposalParent.transactionHash,
-      blockNumber: proposalParent.blockNumber,
-      network: proposalParent.pluginAddress,
-      address: proposalParent.address,
-    } as any
+    for (const proposalParent of parentProposals) {
+      const info = {
+        transactionHash: proposalParent.transactionHash,
+        blockNumber: proposalParent.blockNumber,
+        network: proposalParent.network as NetworksEnum,
+        address: proposalParent.pluginAddress,
+      }
 
-    await ProposalHandler.pairSppProposals(
-      proposalParent,
-      await Models.Plugin.findOne({ address: proposalParent.pluginAddress, network: proposalParent.network }),
-      info,
-    )
+      logger.info('Fixing SPP pair', llo({ info }))
+
+      await ProposalHandler.pairSppProposals(
+        proposalParent,
+        await Models.Plugin.findOne({ address: proposalParent.pluginAddress, network: proposalParent.network }),
+        info as any,
+      )
+
+      const parentProposal = await Models.Proposal.findOne({
+        pluginSubdomain: 'spp',
+        isSubProposal: false,
+        pluginAddress: proposalParent.pluginAddress,
+        proposalIndex: proposalParent.proposalIndex,
+      })
+
+      if (parentProposal && parentProposal.stageIndex !== undefined && parentProposal.subProposals?.length) {
+        logger.info('SPP Stage Indexed Issue Fixed', llo({ info }))
+      } else {
+        logger.error('Failed to fix SPP stage index', llo({ info }))
+      }
+    }
 
     logger.info('End fixSppPair', llo())
   },
