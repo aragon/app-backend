f48e50231f685b2ad3fffba743bcc78904c10df7
cristianizzo <cristianizzo@me.com>
2025-07-01 13:59:26 +0200

    feat: unit test

--- src/modules/proxyProvider/peaqProvider.ts
+++ src/modules/proxyProvider/peaqProvider.ts
@@ -57,62 +57,61 @@ const PeaqProvider: Omit<IWeb3Provider, 'getNativeBalance'> = {
   async fetchAddressTxns({ address, network }: { address: string; network: NetworksEnum }): Promise<any> {
     const assetTransfers = await SubscanApi.getAssetTransfer(address, network)
     const parsedTransfers = await Promise.all(
-      assetTransfers
-        .map(async tx => {
-          const contractAddress = tx.rawContract?.address || utils.zeroAddress
-          const tokenInfo = await ProxyToken.saveAndGetToken(contractAddress, network)
+      assetTransfers.map(async tx => {
+        const contractAddress = tx.rawContract?.address || utils.zeroAddress
+        const tokenInfo = await ProxyToken.saveAndGetToken(contractAddress, network)
 
-          if (!tokenInfo) {
-            logger.error('Token not found', llo({ address, network, contractAddress }))
-            return
-          }
+        if (!tokenInfo) {
+          logger.error('Token not found', llo({ address, network, contractAddress }))
+          return
+        }
 
-          const type = tx.from === address ? ITransactionType.withdraw : ITransactionType.deposit
+        const type = tx.from === address ? ITransactionType.withdraw : ITransactionType.deposit
 
-          if (type === ITransactionType.withdraw && tokenInfo.type === ITokenType.native) {
-            logger.warn('Skipping native withdrawal transaction', llo({ tx, address, network }))
-            return
-          }
+        if (type === ITransactionType.withdraw && tokenInfo.type === ITokenType.native) {
+          logger.warn('Skipping native withdrawal transaction', llo({ tx, address, network }))
+          return
+        }
 
-          const transferLog = {
-            from: tx.from,
-            to: tx.to,
-            value:
-              tx.category === ITransactionCategory.External
-                ? tx.value
-                : ethers.formatUnits(tx.value!, tokenInfo.decimals),
-            blockNum: tx.blockNum,
-            blockTimestamp: tx.blockTimestamp,
-            hash: tx.hash,
-            category: tx.category,
-            uniqueId: tx.uniqueId,
-            rawContract: {
-              address: contractAddress,
-              decimals: tokenInfo.decimals,
-              name: tokenInfo.name,
-              symbol: tokenInfo.symbol,
-              priceUsd: tokenInfo.priceUsd,
-              priceUpdatedAt: tx.blockTimestamp,
-              type: tokenInfo.type,
-            },
-            type,
-          }
-
-          if (TokenUtils.analyzeIfScamToken(tokenInfo?.name || '', tokenInfo?.symbol || '')) {
-            return
-          }
-
-          transferLog.value =
-            tx.category === 'external'
+        const transferLog = {
+          from: tx.from,
+          to: tx.to,
+          value:
+            tx.category === ITransactionCategory.External
               ? tx.value
-              : ethers.formatUnits(tx.value!, tx?.rawContract?.decimals || 0).toString()
+              : ethers.formatUnits(tx.value!, tokenInfo.decimals),
+          blockNum: tx.blockNum,
+          blockTimestamp: tx.blockTimestamp,
+          hash: tx.hash,
+          category: tx.category,
+          uniqueId: tx.uniqueId,
+          rawContract: {
+            address: contractAddress,
+            decimals: tokenInfo.decimals,
+            name: tokenInfo.name,
+            symbol: tokenInfo.symbol,
+            priceUsd: tokenInfo.priceUsd,
+            priceUpdatedAt: tx.blockTimestamp,
+            type: tokenInfo.type,
+          },
+          type,
+        }
 
-          return transferLog
-        })
-        .filter(w => w !== undefined && w !== null),
+        if (TokenUtils.analyzeIfScamToken(tokenInfo?.name || '', tokenInfo?.symbol || '')) {
+          return
+        }
+
+        transferLog.value =
+          tx.category === 'external'
+            ? tx.value
+            : ethers.formatUnits(tx.value!, tx?.rawContract?.decimals || 0).toString()
+
+        return transferLog
+      }),
     )
 
-    return parsedTransfers.filter(Boolean)
+    const filteredTransfers = parsedTransfers.filter(w => w !== undefined && w !== null)
+    return filteredTransfers.filter(Boolean)
   },
 
   fetchTokenPrice: async ({ address, network, pastDays }: any): Promise<any> => {
