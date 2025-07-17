29ff128dc1fe89af986b964bc4433524a43b3df6
ccir <harryburger105@gmail.com>
2025-07-17 21:44:36 +0545

    feat(governance): handle self-delegation in token delegation logic and improve logging

--- src/handlers/governanceVeHandler.ts
+++ src/handlers/governanceVeHandler.ts
@@ -34,6 +34,14 @@ export const GovernanceVeHandler = {
         tokenIds,
       )
 
+      if (fromAddress === toAddress) {
+        logger.verbose(
+          'Self-delegation detected, skipping incoming delegation handling',
+          llo({ info, fromAddress, toAddress }),
+        )
+        return
+      }
+
       await GovernanceVeHandler._handleTokenDelegation(
         parsedEvent,
         info,
--- src/services/aragon-plugins/index.ts
+++ src/services/aragon-plugins/index.ts
@@ -56,7 +56,7 @@ const AragonPluginsService: IService = {
           })
 
           if ((token?.type === ITokenType.ERC20 || token?.type === ITokenType.escrowAdapter) && token.isGovernance) {
-            logger.info('Sync plugin: token is ERC20', llo({ plugin: plugin.address, token: token.address }))
+            logger.info(`Sync plugin: token is ${token.type}`, llo({ plugin: plugin.address, token: token.address }))
 
             await LogTokenVoting.start(plugin, token, isHistorical)
           } else {
