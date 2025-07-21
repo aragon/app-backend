408b4ed82c71718e0a5679f38c221c69c386f598
Sishir Pokhrel <98728151+harryburger@users.noreply.github.com>
2025-07-21 23:35:28 +0545

    feat(evmExplorer): implement fallback logic for fetching contract creation and source code (#761)
    * feat(evmExplorer): implement fallback logic for fetching contract creation and source code
    
    * feat(etherscan): remove redundant tests for fetchContractSourceCode and fetchContractCreation
    
    * feat(etherscan): remove the routescan test in dep
    
    * feat(daoMemberMapping): increase coverage

--- src/helpers/routeScanHelper.ts
+++ src/helpers/routeScanHelper.ts
@@ -1,11 +1,10 @@
 import logger from '@logger'
 import axios from 'axios'
 import config from '@config'
-import { type IEtherScanSource, type IWeb3ContractCreation, type NetworksEnum } from '@types'
+import { type NetworksEnum } from '@types'
 import { retryRequest } from '@helpers/retryRequest'
 import BottleneckModule from '@modules/bottleneck'
 import ProviderModule from '@modules/provider'
-import Web3Helper from '@helpers/web3'
 
 const llo = logger.logMeta.bind(null, { service: 'helpers:RouteScanHelper' })
 
@@ -31,65 +30,6 @@ const RouteScanHelper = {
     } catch (error) {
       logger.error('Error in RouteScan API call', llo({ error }))
       throw error
-    }
-  },
-
-  fetchContractSourceCode: async ({ address, network }): Promise<IEtherScanSource[] | null> => {
-    const params = {
-      module: 'contract',
-      action: 'getsourcecode',
-      address,
-    }
-
-    try {
-      const result = await RouteScanHelper._rpCall(params, network)
-      if (
-        result.status === '1' &&
-        result.message === 'OK' &&
-        result.result.length > 0 &&
-        result.result[0].SourceCode !== ''
-      ) {
-        return [
-          {
-            SourceCode: result.result[0].SourceCode,
-            ContractName: result.result[0].ContractName,
-            ABI: result.result[0].ABI,
-          },
-        ]
-      }
-    } catch (error) {
-      logger.error('Error fetchContractSourceCode from RouteScan', llo({ params, network, error }))
-    }
-    return null
-  },
-
-  fetchContractCreation: async ({ address, network }): Promise<IWeb3ContractCreation> => {
-    const params = {
-      module: 'contract',
-      action: 'getcontractcreation',
-      contractaddresses: address,
-    }
-
-    try {
-      const result = await RouteScanHelper._rpCall(params, network)
-      if (result.status === '1' && result.message === 'OK' && result.result.length > 0) {
-        const response = {
-          address: result.result[0].contractAddress,
-          transactionHash: result.result[0].txHash,
-          blockNumber: 0,
-        }
-
-        const txReceipt = await Web3Helper.getTransaction(response.transactionHash, network)
-        response.blockNumber = txReceipt?.blockNumber || 0
-        return response
-      }
-    } catch (error) {
-      logger.error('Error fetchContractCreation from RouteScan', llo({ params, network, error }))
-    }
-    return {
-      address,
-      transactionHash: '',
-      blockNumber: 0,
     }
   },
 
