062905dc96be5243477c1c770dda33bc8e03cc5f
ccir <harryburger105@gmail.com>
2025-07-22 09:12:11 +0545

    feat(governance): mongo no index

--- config/common.ts
+++ config/common.ts
@@ -420,6 +420,7 @@ const getConfigObject = (sourceConfig: Record<string, any>): IConfig => {
       CONNECTION_RETRY: utils.configParser(sourceConfig, 'number', 'MONGO_DB_RETRY_CONNECTION_RETRY', 60),
       CONNECTION_TIMEOUT: utils.configParser(sourceConfig, 'number', 'MONGO_DB_RETRY_CONNECTION_TIMEOUT', 5000),
       CONNECTION_DELAY: utils.configParser(sourceConfig, 'number', 'MONGO_DB_RETRY_CONNECTION_DELAY', 1000),
+      UPDATE_INDEXES: utils.configParser(sourceConfig, 'bool', 'MONGO_DB_UPDATE_INDEXES', true),
     },
 
     LOG: {
