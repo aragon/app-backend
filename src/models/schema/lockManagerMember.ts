e2ee3d2741c0b97d6b5998213a54e733d029ff3e
ccir <harryburger105@gmail.com>
2025-07-30 10:53:05 +0545

    feat(lock-manager): implement pagination for LockManagerMember and enhance member retrieval logic

--- src/models/schema/lockManagerMember.ts
+++ src/models/schema/lockManagerMember.ts
@@ -1,8 +1,18 @@
 import { index, modelOptions, prop } from '@typegoose/typegoose'
-import { HexAddress, ICollectionNames, NetworksEnum, type ILockManagerMemberIdParams } from '@types'
+import {
+  HexAddress,
+  ICollectionNames,
+  NetworksEnum,
+  type ILockManagerMemberIdParams,
+  type IPaginatedResult,
+  type IPaginationParams,
+  type IMembersResponse,
+} from '@types'
 import { Model, type SaveOptions } from 'mongoose'
 import * as _ from 'lodash'
 import { assert } from '@errors'
+import ModelUtils from '@models/utils/models'
+import { AggregationQueryHelper } from '@models/utils/aggregation'
 
 const customName = ICollectionNames.LockManagerMember
 
@@ -122,5 +132,126 @@ export default class LockManagerMember extends Model {
 
   async reload(tOpts?: SaveOptions) {
     return await this.model(customName).findById(this._id, tOpts)
+  }
+
+  static async findAndPaginate({
+    paginationParams = {},
+    pluginAddress,
+    network,
+  }: {
+    paginationParams?: IPaginationParams
+    pluginAddress: HexAddress
+    network: NetworksEnum
+  }): Promise<IPaginatedResult<IMembersResponse>> {
+    const request = ModelUtils.paginateAndSort(paginationParams)
+    const filter = {
+      pluginAddress,
+      network,
+      isActive: true,
+    }
+
+    const currentPage = request.skip / request.limit + 1
+
+    const query = [
+      AggregationQueryHelper.member(
+        {
+          memberAddress: '$memberAddress',
+        },
+        'member',
+        {
+          address: 1,
+          ens: 1,
+          avatar: 1,
+        },
+      ),
+      {
+        $addFields: {
+          member: {
+            $cond: {
+              if: { $gt: [{ $size: '$member' }, 0] },
+              then: {
+                address: { $arrayElemAt: ['$member.address', 0] },
+                ens: { $arrayElemAt: ['$member.ens', 0] },
+                avatar: { $arrayElemAt: ['$member.avatar', 0] },
+              },
+              else: {
+                address: '$memberAddress',
+                ens: null,
+                avatar: null,
+              },
+            },
+          },
+        },
+      },
+      AggregationQueryHelper.memberMetrics(
+        {
+          memberAddress: '$memberAddress',
+          network,
+          pluginAddress,
+        },
+        'memberMetrics',
+        {
+          _id: 0,
+          lastActivity: 1,
+          firstActivity: 1,
+          delegateReceivedCount: 1,
+          voteCount: 1,
+          proposalCount: 1,
+        },
+      ),
+      {
+        $project: {
+          _id: 0,
+          address: '$member.address',
+          ens: '$member.ens',
+          avatar: '$member.avatar',
+          votingPower: 1,
+          metrics: {
+            $ifNull: [
+              {
+                $arrayElemAt: ['$memberMetrics', 0],
+              },
+              {
+                lastActivity: null,
+                firstActivity: null,
+                delegateReceivedCount: 0,
+                voteCount: 0,
+                proposalCount: 0,
+              },
+            ],
+          },
+        },
+      },
+    ]
+
+    const aggQuery = [
+      { $match: filter },
+      ...query,
+      { $sort: request?.sort },
+      { $skip: request?.skip },
+      { $limit: request?.limit },
+    ]
+
+    const [data, totalRecords] = await Promise.all([
+      this.aggregate(aggQuery),
+      this.aggregate([{ $match: filter }, { $count: 'totalRecords' }]),
+    ])
+
+    const _totalRecords = totalRecords && totalRecords.length === 1 ? totalRecords[0].totalRecords : 0
+    const totalPages = Math.ceil(_totalRecords / request.limit)
+
+    if (currentPage > totalPages) {
+      return ModelUtils.paginateEmptyResponse(request.limit)
+    }
+
+    return {
+      metadata: {
+        page: currentPage,
+        pageSize: request.limit,
+        totalPages,
+        totalRecords: _totalRecords,
+      },
+      data: data as any,
+    }
   }
 }
