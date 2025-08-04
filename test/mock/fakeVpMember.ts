1801dbb313ed98f27448f706f2c77114e2365955
cristianizzo <cristianizzo@me.com>
2025-08-04 03:08:46 +0200

    feat: refactor governances + unit test

---
+++ test/mock/fakeVpMember.ts
@@ -0,0 +1,25 @@
+import { NetworksEnum } from '@types'
+
+export const fakeVpMembers = [
+  {
+    memberAddress: '0x123456789012345678901234567890123456789A',
+    tokenAddress: '0xA23456789012345678901234567890123456789B',
+    network: NetworksEnum.ethereumMainnet,
+    votingPower: '1000000000000000000000',
+    delegateReceivedCount: 5,
+  },
+  {
+    memberAddress: '0x223456789012345678901234567890123456789A',
+    tokenAddress: '0xA23456789012345678901234567890123456789B',
+    network: NetworksEnum.ethereumMainnet,
+    votingPower: '500000000000000000000',
+    delegateReceivedCount: 2,
+  },
+  {
+    memberAddress: '0x323456789012345678901234567890123456789A',
+    tokenAddress: '0xA23456789012345678901234567890123456789B',
+    network: NetworksEnum.ethereumMainnet,
+    votingPower: '0',
+    delegateReceivedCount: 0,
+  },
+]
