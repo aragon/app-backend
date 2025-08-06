275e3c0b2598de3dabe9c36b2c29747fec6ab247
cristianizzo <cristianizzo@me.com>
2025-08-06 21:30:43 +0200

    fix: can createproposal vp

--- test/unit/services/aragon-dao/memberInfo.spec.ts
+++ test/unit/services/aragon-dao/memberInfo.spec.ts
@@ -612,7 +612,7 @@ describe('AragonDao: memberInfo', () => {
 
       const settingsStub = sandbox.stub(Models.Setting, 'findActive').resolves({} as any)
 
-      const getLockedBalanceStub = sandbox.stub(LockToVoteHelper, 'getUserLockedBalance').resolves(0n)
+      const getLockedBalanceStub = sandbox.stub(LockToVoteHelper, 'getUserLockedBalance').resolves(0)
 
       const result = await MemberInfo.canCreateProposal(
         '0xPluginAddress',
@@ -639,7 +639,7 @@ describe('AragonDao: memberInfo', () => {
 
       const settingsStub = sandbox.stub(Models.Setting, 'findActive').resolves({} as any)
 
-      const getLockedBalanceStub = sandbox.stub(LockToVoteHelper, 'getUserLockedBalance').resolves(100n)
+      const getLockedBalanceStub = sandbox.stub(LockToVoteHelper, 'getUserLockedBalance').resolves(100)
 
       const result = await MemberInfo.canCreateProposal(
         '0xPluginAddress',
@@ -666,7 +666,7 @@ describe('AragonDao: memberInfo', () => {
 
       const settingsStub = sandbox.stub(Models.Setting, 'findActive').resolves({} as any)
 
-      const getLockedBalanceStub = sandbox.stub(LockToVoteHelper, 'getUserLockedBalance').resolves(1000000n)
+      const getLockedBalanceStub = sandbox.stub(LockToVoteHelper, 'getUserLockedBalance').resolves(1000000)
 
       const result = await MemberInfo.canCreateProposal(
         '0xPluginAddress',
