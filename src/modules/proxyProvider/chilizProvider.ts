42fe2b281d3d4005ce4e4506c0feebc407b1d0e8
ccir <harryburger105@gmail.com>
2025-08-12 23:16:30 +0545

    feat(chilizProvider): reorder explorers in fetchContractCreation and improve token logging

--- src/modules/proxyProvider/chilizProvider.ts
+++ src/modules/proxyProvider/chilizProvider.ts
@@ -55,7 +55,7 @@ const ChilizProvider: Omit<IWeb3Provider, 'getNativeBalance'> & {
   },
 
   fetchContractCreation: async ({ address, network }) => {
-    const explorers = [EvmExplorerEnum.CHILIZ, EvmExplorerEnum.ROUTESCAN]
+    const explorers = [EvmExplorerEnum.ROUTESCAN, EvmExplorerEnum.CHILIZ]
 
     const result = await utils.fallbackCall(
       explorers,
