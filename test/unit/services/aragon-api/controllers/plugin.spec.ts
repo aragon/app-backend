a0bf36a4a55deca53ef844baabe2b6c1c4eeeeb1
ccir <harryburger105@gmail.com>
2025-08-18 10:12:06 +0545

    feat(tests): coverage

--- test/unit/services/aragon-api/controllers/plugin.spec.ts
+++ test/unit/services/aragon-api/controllers/plugin.spec.ts
@@ -5,12 +5,15 @@ import PluginController from '@services/aragon-api/controllers/plugins'
 import RabbitMQHelper from '@helpers/rabbitMQ'
 import config from '@config'
 import { NetworksEnum, EnumQueueName } from '@types'
+import logger from '@logger'
 
 describe('Controller: Plugin', () => {
   let sandbox: SinonSandbox
+  let loggerWarnStub: sinon.SinonStub
 
   beforeEach(() => {
     sandbox = sinon.createSandbox()
+    loggerWarnStub = sandbox.stub(logger, 'warn')
   })
 
   afterEach(() => {
@@ -40,6 +43,27 @@ describe('Controller: Plugin', () => {
         timeout: config.RABBITMQ.TIMEOUT,
       })
       expect(result).to.equal(installationData)
+    })
+
+    it('should log error and re-throw when RabbitMQ sendMessage fails', async () => {
+      const rabbitMqError = new Error('RabbitMQ connection failed')
+      const rabbitMqStub = sandbox.stub(RabbitMQHelper, 'sendMessage').rejects(rabbitMqError)
+
+      await expect(PluginController.getInstallationData({
+        pluginAddress,
+        network,
+      })).to.be.rejectedWith(Error, 'RabbitMQ connection failed')
+
+      expect(rabbitMqStub.calledOnce).to.be.true
+      expect(loggerWarnStub.calledOnce).to.be.true
+      expect(loggerWarnStub.calledWith('Error while getting plugin installation data')).to.be.true
+      
+      const logCall = loggerWarnStub.getCall(0)
+      expect(logCall.args[1]).to.deep.include({
+        error: rabbitMqError,
+        pluginAddress,
+        network,
+      })
     })
   })
 })
