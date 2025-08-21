321b894ab31175b5642f797fb84880c7f2d0ac5f
ccir <harryburger105@gmail.com>
2025-08-21 09:08:23 +0545

    feat: update capital distribution tests and enhance plugin metadata handling

--- test/unit-dep/capitalDistributor/capitalDistributor.spec.ts
+++ test/unit-dep/capitalDistributor/capitalDistributor.spec.ts
@@ -25,62 +25,65 @@ describe('Capital Distributor', () => {
     const network = NetworksEnum.ethereumSepolia
     const pluginAddress = '0x5dA61302D0d08d80D39f015b75595052fD4CdD06'
 
-    const addressesses = [{
-      campaignId: '0',
-      addresses: [
-        {
-          address: '0x17366cae2b9c6C3055e9e3C78936a69006BE5409',
-          amount: '2068407131086655006',
-        },
-        {
-          address: '0xF6ad40D5D477ade0C640eaD49944bdD0AA1fBF05',
-          amount: '3066005202605466524'
-        },
-        {
-          address: '0x764a31E070c6Ea2E81CbC1f680BF9a07f762ED2c',
-          amount: '3327258466775317797'
-        },
-        {
-          address: '0x00C51Fad10462780e488B54D413aD92B28b88204',
-          amount: '4301960492862894413'
-        },
-        {
-          address: '0xD740fd724D616795120BC363316580dAFf41129A',
-          amount: '4429543088946624610'
-        },
-      ],
-    }, {
-      campaignId: '1',
-      addresses: [
-        {
-          address: '0x17366cae2b9c6C3055e9e3C78936a69006BE5409',
-          amount: '5931865400721184342',
-        },
-        {
-          address: '0xF6ad40D5D477ade0C640eaD49944bdD0AA1fBF05',
-          amount: '7315096276842113998'
-        },
-        {
-          address: '0x764a31E070c6Ea2E81CbC1f680BF9a07f762ED2c',
-          amount: '8743269015224678811'
-        },
-        {
-          address: '0x00C51Fad10462780e488B54D413aD92B28b88204',
-          amount: '6674012549813207563'
-        },
-        {
-          address: '0xD740fd724D616795120BC363316580dAFf41129A',
-          amount: '9152804736221590487'
-        },
-      ],
-    }]
+    const addressesses = [
+      {
+        campaignId: '0',
+        addresses: [
+          {
+            address: '0x17366cae2b9c6C3055e9e3C78936a69006BE5409',
+            amount: '2068407131086655006',
+          },
+          {
+            address: '0xF6ad40D5D477ade0C640eaD49944bdD0AA1fBF05',
+            amount: '3066005202605466524',
+          },
+          {
+            address: '0x764a31E070c6Ea2E81CbC1f680BF9a07f762ED2c',
+            amount: '3327258466775317797',
+          },
+          {
+            address: '0x00C51Fad10462780e488B54D413aD92B28b88204',
+            amount: '4301960492862894413',
+          },
+          {
+            address: '0xD740fd724D616795120BC363316580dAFf41129A',
+            amount: '4429543088946624610',
+          },
+        ],
+      },
+      {
+        campaignId: '1',
+        addresses: [
+          {
+            address: '0x17366cae2b9c6C3055e9e3C78936a69006BE5409',
+            amount: '5931865400721184342',
+          },
+          {
+            address: '0xF6ad40D5D477ade0C640eaD49944bdD0AA1fBF05',
+            amount: '7315096276842113998',
+          },
+          {
+            address: '0x764a31E070c6Ea2E81CbC1f680BF9a07f762ED2c',
+            amount: '8743269015224678811',
+          },
+          {
+            address: '0x00C51Fad10462780e488B54D413aD92B28b88204',
+            amount: '6674012549813207563',
+          },
+          {
+            address: '0xD740fd724D616795120BC363316580dAFf41129A',
+            amount: '9152804736221590487',
+          },
+        ],
+      },
+    ]
 
     await Utils.handleEventsFromTxHashes(
       ['0x11faef71292d3fe642e87f00a4ef0776c0fe8f71e07029ef595535704cab04cd'],
       network,
     )
 
-    for(const address of addressesses) {
+    for (const address of addressesses) {
       await CapitalDistributorAdminController.uploadMembersList({
         campaignId: address.campaignId,
         pluginAddress,
@@ -101,7 +104,7 @@ describe('Capital Distributor', () => {
 
     await LogCapitalDistributor.start(plugin)
 
-    for(const address of addressesses) {
+    for (const address of addressesses) {
       const totalRewards = address.addresses.reduce((acc, curr) => acc + BigInt(curr.amount), BigInt(0))
       const campaignDetail = await Models.Campaign.findOne({
         pluginAddress: plugin.address,
@@ -113,7 +116,7 @@ describe('Capital Distributor', () => {
     }
 
     const rewardReceivedUsers = await Models.CampaignReward.find({
-      claims: { $exists: true, $ne: [] }
+      claims: { $exists: true, $ne: [] },
     })
 
     const campaignStatus = await CapitalDistributorController.getUserCampaignStatus(
@@ -143,23 +146,17 @@ describe('Capital Distributor', () => {
     }, BigInt(0))
     expect(notClaimedRewardStat.totalClaimable).to.be.eq(totalRewardsForUser.toString())
 
-    const daoDetails = await DaoController.getDaoByAddress(
-      plugin.daoAddress,
-      network,
-    )
+    const daoDetails = await DaoController.getDaoByAddress(plugin.daoAddress, network)
 
     const pluginFromResponse = daoDetails.plugins.find(
-      (pl: any) => pl.interfaceType === IPluginInterfaceType.capitalDistributor
+      (pl: any) => pl.interfaceType === IPluginInterfaceType.capitalDistributor,
     ) as any
 
     expect(pluginFromResponse).to.exist
 
     expect(pluginFromResponse.isSupported).to.be.true
 
-    expect(pluginFromResponse.blockedCountries).to.deep.eq([
-      "RU",
-      "US",
-    ])
+    expect(pluginFromResponse.blockedCountries).to.deep.eq(['RU', 'US'])
     expect(pluginFromResponse.enableOfacCheck).to.be.true
     expect(pluginFromResponse.termsConditionsUrl).to.be.eq('https://capital-distributor.com/terms')
     expect(pluginFromResponse.isBody).to.be.eq(false)
