14e9b1c7dd29afcc7277564fcd20801cfc9f12ce
ccir <harryburger105@gmail.com>
2025-08-28 14:51:10 +0545

    feat: clean up

--- src/types/db.ts
+++ src/types/db.ts
@@ -26,7 +26,6 @@ import type Campaign from '@models/schema/campaign'
 import type CampaignReward from '@models/schema/campaignReward'
 import type LockToVoteMember from '@models/schema/lockToVoteMember'
 import type Migration from '@models/schema/migration'
-import type Simulation from '@models/schema/simulation'
 
 export interface IMongoModel {
   Migration: typeof Migration
@@ -57,7 +56,6 @@ export interface IMongoModel {
   PluginMember: typeof PluginMember
   PluginMetrics: typeof PluginMetrics
   LockToVoteMember: typeof LockToVoteMember
-  Simulation: typeof Simulation
 }
 
 export enum ICollectionNames {
@@ -89,7 +87,6 @@ export enum ICollectionNames {
   PluginMember = 'PluginMember',
   PluginMetrics = 'PluginMetrics',
   LockToVoteMember = 'LockToVoteMember',
-  Simulation = 'Simulation',
 }
 
 export enum ITransactionIndexCheckType {
