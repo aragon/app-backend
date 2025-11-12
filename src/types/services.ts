e4a9873b39933965bee9eabb56fc05bc8490ed1d
ccir <harryburger105@gmail.com>
2025-11-12 14:46:11 +0545

    feat(migration): add ARAGON_MIGRATION service name to MigrationService

--- src/types/services.ts
+++ src/types/services.ts
@@ -14,6 +14,7 @@ export enum EnumServiceName {
   ARAGON_PLUGINS = 'aragon-plugins',
   ARAGON_RATES = 'aragon-rates',
   ARAGON_REQUEUE = 'aragon-requeue',
+  ARAGON_MIGRATION = 'aragon-migration',
 }
 
 export interface IMigration {
