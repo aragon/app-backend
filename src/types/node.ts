417c5fd258a0fb3119a496d34503f8490edeb6d6
cristianizzo <cristianizzo@me.com>
2025-12-01 16:24:57 +0100

    feat(provider): migrate to DRPC

--- src/types/node.ts
+++ src/types/node.ts
@@ -9,6 +9,7 @@ export interface IAlchemyConfig {
 export enum IProviderType {
   ALCHEMY = 'alchemy', // must be lowercase
   ARAGON = 'aragon', // must be lowercase
+  DRPC = 'drpc', // must be lowercase
 }
 
 export enum IConnectionType {
@@ -20,6 +21,14 @@ export interface IAragonNodeConfig extends Omit<IAlchemyConfig, 'alchemyApiKey'>
   rpcEndpoint: string
 }
 
+export interface IDrpcConfig {
+  providerType: IProviderType
+  drpcApiKey: string
+  fromBlock: number
+  confirmationBlocks: number
+  intervalBlockTime: number
+}
+
 export interface INodeConnection {
   rpc: any
 }
@@ -28,7 +37,12 @@ export interface IAlchemyNodeConnection {
   rpc: any
 }
 
+export interface IDrpcNodeConnection {
+  rpc: any
+}
+
 export interface IProviderProxy {
   alchemy?: IAlchemyNodeConnection
   aragon?: INodeConnection
+  drpc?: IDrpcNodeConnection
 }
