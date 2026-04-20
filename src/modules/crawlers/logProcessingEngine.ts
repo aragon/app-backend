fd2aacc639e84ee866298e4da07262c895e0393f
ccir <harryburger105@gmail.com>
2026-04-21 00:30:29 +0545

    feat(logProcessing): narrow TickContext logs to parsed events

--- src/modules/crawlers/logProcessingEngine.ts
+++ src/modules/crawlers/logProcessingEngine.ts
@@ -203,7 +203,7 @@ export class LogProcessingEngine {
 
     if (parsed.length === 0) return highestBlockNumber
 
-    const tickCtx = new TickContext(this.network, logs)
+    const tickCtx = new TickContext(this.network, parsed.map(event => event.log ))
     if (this.isRealtimeStrategy(strategy)) {
       await tickCtx.init()
     }
@@ -376,7 +376,7 @@ export class LogProcessingEngine {
       return 0
     }
 
-    const tickCtx = new TickContext(this.network, logs)
+    const tickCtx = new TickContext(this.network, [])
     if (this.isRealtimeStrategy(strategy)) {
       await tickCtx.init()
     }
