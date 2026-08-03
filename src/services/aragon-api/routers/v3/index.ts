68af91578d5d0632958af59effebe8e233e59b78
Milos Dzepina <milos@aragon.org>
2026-08-03 13:24:06 +0200

    feat: implement V3 endpoint for lightweight actions decoding without DAO context
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/services/aragon-api/routers/v3/index.ts
+++ src/services/aragon-api/routers/v3/index.ts
@@ -1,11 +1,16 @@
 import Router from '@koa/router'
+import ContractRouter from './contract'
 import DaoRouter from './dao'
 
 const V3Router = {
   router(): Router {
     const router = new Router()
     const daoRouter = DaoRouter.router()
+    const contractRouter = ContractRouter.router()
+
     router.use('/daos', daoRouter.routes(), daoRouter.allowedMethods())
+    router.use('/contract', contractRouter.routes(), contractRouter.allowedMethods())
+
     return router
   },
 }
