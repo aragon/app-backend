e669d4944be8e442cbc95e0e9d81918f0e4c0d95
ccir <harryburger105@gmail.com>
2026-09-24 21:28:48 +0545

    feat(APP-1165): registerSafeProcesses tool repairs missed grants and applies the condition

--- tools/toolLauncher.ts
+++ tools/toolLauncher.ts
@@ -24,6 +24,7 @@ import ToolsMissingSlugs, { MissingSlugs } from '@tools/missingSlugs'
 import Queue from '@tools/queue'
 import RefetchDaoMetrics from '@tools/refetchDaoMetrics'
 import RefetchProposalsMetrics from '@tools/refetchProposalMetrics'
+import RegisterSafeProcesses from '@tools/registerSafeProcesses'
 import ResyncDaoVeGovernance from '@tools/resyncDaoVeGovernance'
 import RevertDbAtBlock from '@tools/revertDbAtBlock'
 import RewardGenerator from '@tools/rewardGenerator'
@@ -84,6 +85,7 @@ const runners = {
   BackfillObjectionFlag,
   BackfillCrossChain,
   MissingSlugs,
+  RegisterSafeProcesses,
 }
 
 const appToRun = runners[TOOL_RUN!]
